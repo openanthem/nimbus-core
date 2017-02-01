f990551ee2f7c0258383421ba7ce980ae9ff7a28
Rohit Bajaj <rohit.bajaj@anthem.com>
2017-01-31 22:32:17 -0500

    NIM-3444 OSS changes

--- platform.core.process/src/test/java/com/anthem/nimbus/platform/core/process/api/AbstractPlatformSpringTests.java
+++ platform.core.process/src/test/java/com/anthem/nimbus/platform/core/process/api/AbstractPlatformSpringTests.java
@@ -5,16 +5,21 @@ package com.anthem.nimbus.platform.core.process.api;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.anthem.nimbus.platform.core.process.PlatformProcessEngineConfiguration;
+
 
 /**
  * @author Soham Chakravarti
  *
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("local")
+@ComponentScan(basePackages={"com.anthem.nimbus.platform.utils.converter", "com.anthem.nimbus.platform.core.process", "com.anthem.nimbus.platform.client.extension.cm"})
+@SpringBootTest(classes=PlatformProcessEngineConfiguration.class)
+@ActiveProfiles("test")
 public abstract class AbstractPlatformSpringTests {
-
+	
 }
