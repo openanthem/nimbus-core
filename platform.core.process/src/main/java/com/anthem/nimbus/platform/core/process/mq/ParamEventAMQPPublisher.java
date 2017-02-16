1ad891984437d270c1affc9a228304bbf17a2a3b
Rakeshkumar Patel <AC67870@lc02r50wgg8wm.us.ad.wellpoint.com>
2017-02-16 18:02:59 -0500

    Added web socket code

--- platform.core.process/src/main/java/com/anthem/nimbus/platform/core/process/mq/ParamEventAMQPPublisher.java
+++ platform.core.process/src/main/java/com/anthem/nimbus/platform/core/process/mq/ParamEventAMQPPublisher.java
@@ -6,9 +6,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import com.anthem.nimbus.platform.spec.contract.event.StateAndConfigEventPublisher;
 import com.anthem.oss.nimbus.core.domain.command.execution.CommandTransactionInterceptor;
 import com.anthem.oss.nimbus.core.domain.command.execution.ExecuteOutput;
 import com.anthem.oss.nimbus.core.domain.command.execution.MultiExecuteOutput;
+import com.anthem.oss.nimbus.core.domain.model.state.EntityState;
 import com.anthem.oss.nimbus.core.domain.model.state.EntityState.Model;
 import com.anthem.oss.nimbus.core.domain.model.state.EntityState.Param;
 import com.anthem.oss.nimbus.core.domain.model.state.ModelEvent;
@@ -20,20 +22,25 @@ import com.anthem.oss.nimbus.core.domain.model.state.internal.AbstractEvent.Supp
  */
 
 @Component
-public class ParamEventAMQPPublisher { //implements StateAndConfigEventPublisher {
+public class ParamEventAMQPPublisher implements StateAndConfigEventPublisher {
 
-//	@Autowired
-//	SimpMessagingTemplate messageTemplate;
-//	
-//	@Autowired CommandTransactionInterceptor interceptor;
-//	
-//	@Override
-//	public boolean shouldSuppress(SuppressMode mode) {
-//		return SuppressMode.ECHO == mode;
-//	}	
-//	
-//	@Override
-//	public boolean shouldAllow(StateAndConfig<?,?> p) {
+	@Autowired
+	SimpMessagingTemplate messageTemplate;
+	
+	@Autowired CommandTransactionInterceptor interceptor;
+	
+	@Override
+	public boolean shouldSuppress(SuppressMode mode) {
+		return SuppressMode.ECHO == mode;
+	}	
+	
+	@Override
+	public boolean shouldAllow(EntityState<?> p) {
+		
+		if(p.getConfig().isMapped()){
+			return true;
+		}
+		return false;
 //		if(p instanceof Param<?>) {
 //			Param<?> param = (Param<?>) p;
 //			return param.getConfig().isView();
@@ -42,40 +49,39 @@ public class ParamEventAMQPPublisher { //implements StateAndConfigEventPublisher
 //			Model<?,?> param = (Model<?,?>) p;
 //			return param.getConfig().isView();
 //		}
-//	}
-//	
-//	@Override
-//	public boolean publish(ModelEvent<StateAndConfig<?,?>> modelEvent) {
-//		StateAndConfig<?,?>  p = modelEvent.getPayload();
-//		
+	}
+	
+	@Override
+	public boolean publish(ModelEvent<Param<?>> modelEvent) {
+		Param<?>  p = modelEvent.getPayload();		
 //		//TODO temp impl for GRID collection handling
-////		if(p.getConfig() instanceof ViewParamConfig) {
-////			AnnotationConfig uiStyle = ((ViewParamConfig)p.getConfig()).getUiStyles();
-////	
-////			if(uiStyle!=null && ClassUtils.getShortName(Grid.class).equals(uiStyle.getName())) {
-////				Object state = p.getState();
-////				ModelEvent e = new ModelEvent(Action._replace, p instanceof Param<?> ?((Param<?>)p).getPath():((Model<?,?>)p).getPath(), state);
-////				publishInternal(e);
-////				return true;
-////			}
-////	
-////		}
-//
-//		return publishInternal(modelEvent);
-//	}
+//		if(p.getConfig() instanceof ViewParamConfig) {
+//			AnnotationConfig uiStyle = ((ViewParamConfig)p.getConfig()).getUiStyles();
 //	
-//	protected boolean publishInternal(ModelEvent<?> result) {
-//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//		ExecuteOutput<ModelEvent<?>> executeOutput = new ExecuteOutput<>();
-//		executeOutput.setResult(result);
-//		
-//		MultiExecuteOutput multiExecOutput = interceptor.handleResponse(executeOutput);
-//		
-//		messageTemplate.convertAndSend("/queue/updates", multiExecOutput); // TODO get the destination name from the config server
-//		
-//		//messageTemplate.convertAndSendToUser(auth.getName(),"/queue/updates", executeOutput); // TODO get the destination name from the config server
-//		
-//		return true;
-//	}
+//			if(uiStyle!=null && ClassUtils.getShortName(Grid.class).equals(uiStyle.getName())) {
+//				Object state = p.getState();
+//				ModelEvent e = new ModelEvent(Action._replace, p instanceof Param<?> ?((Param<?>)p).getPath():((Model<?,?>)p).getPath(), state);
+//				publishInternal(e);
+//				return true;
+//			}
+//	
+//		}
+//
+		return publishInternal(modelEvent);
+	}
+	
+	protected boolean publishInternal(ModelEvent<Param<?>> result) {
+		//Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		ExecuteOutput<ModelEvent<?>> executeOutput = new ExecuteOutput<>();
+		executeOutput.setResult(result);
+		
+		MultiExecuteOutput multiExecOutput = interceptor.handleResponse(executeOutput);
+		
+		messageTemplate.convertAndSend("/queue/updates", multiExecOutput); // TODO get the destination name from the config server
+		
+		//messageTemplate.convertAndSendToUser(auth.getName(),"/queue/updates", executeOutput); // TODO get the destination name from the config server
+		
+		return true;
+	}
 
 }
