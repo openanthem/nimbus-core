f1e50751fbbce00eedcf09579f369d66f73d71a1
Rakeshkumar Patel <AC67870@va1efs01.agpcom.local>
2017-03-15 14:22:18 -0400

    Added a change to fix the different behavior between application ran from commandline vs ide Note: not sure why this behavior occurs, just putting a band aid for now

--- platform.core.process/src/main/java/com/anthem/oss/nimbus/core/domain/command/execution/CommandTransactionInterceptor.java
+++ platform.core.process/src/main/java/com/anthem/oss/nimbus/core/domain/command/execution/CommandTransactionInterceptor.java
@@ -24,6 +24,12 @@ public class CommandTransactionInterceptor {
 	}
 	
 	public MultiExecuteOutput handleResponse(ExecuteOutput<?> rawResult) {
+		// This was done to handle a scenario where the result of the executeOutput had a state = MultiExecuteOutput
+		// in that case, it was wrapping the response into the outer EXECUTE behavior due to this implementation. 
+		// so checking if the result is of Type Holder then calling the handleResponse(Holder) method
+		if(rawResult.getResult() instanceof Holder) {
+			return handleResponse(((Holder)rawResult.getResult()));
+		}
 		ExecuteOutput.BehaviorExecute<?> bExec = new ExecuteOutput.BehaviorExecute<>(Behavior.$execute, rawResult.getResult());
 		bExec.setExecuteException(rawResult.getExecuteException());
 		bExec.setValidationResult(rawResult.getValidationResult());
