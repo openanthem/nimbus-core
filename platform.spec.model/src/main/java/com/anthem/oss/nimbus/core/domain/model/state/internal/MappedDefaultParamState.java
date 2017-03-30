aaf61d40214b01b51c568ad4d52abd3b979bb516
Soham Chakravarti <soham.chakravarti@anthem.com>
2017-03-30 11:12:27 -0400

    leaf-state - initial

--- platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/domain/model/state/internal/MappedDefaultParamState.java
+++ platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/domain/model/state/internal/MappedDefaultParamState.java
@@ -36,4 +36,25 @@ public class MappedDefaultParamState<T, M> extends DefaultParamState<T> implemen
 		
 		getMapsTo().registerSubscriber(this);
 	}
+	
+	
+	/**
+	 * resurrect entity pojo state from model/param state
+	 * @return
+	 */
+//	public T getState2() {
+//		if(!isNested())
+//			return getState();
+//		
+//		// create new entity instance
+//		T entity = getProvider().getParamStateGateway()._instantiate(this);
+//		
+//		// assign param values to entity instance attributes
+//		findIfNested().getParams().stream()
+//			.filter(p->!p.isNested())
+//			
+//		;
+//		
+//		return entity;
+//	}
 }
