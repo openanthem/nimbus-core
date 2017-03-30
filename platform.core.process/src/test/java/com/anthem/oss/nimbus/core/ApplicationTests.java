823b6ad339fbe400c1eb386c52a72ed0ce9f21cf
Rakesh Patel <rakeshkumar.patel@anthem.com>
2017-03-29 23:06:03 -0400

    .

--- platform.core.process/src/test/java/com/anthem/oss/nimbus/core/ApplicationTests.java
+++ platform.core.process/src/test/java/com/anthem/oss/nimbus/core/ApplicationTests.java
@@ -2,21 +2,18 @@ package com.anthem.oss.nimbus.core;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.anthem.oss.nimbus.core.config.Neo4jConfig;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Profile("test")
-@EnableAutoConfiguration
+@ActiveProfiles("test")
 public class ApplicationTests {
-
+	
 	@Test
 	public void contextLoads() {
+		
 	}
 
 }
