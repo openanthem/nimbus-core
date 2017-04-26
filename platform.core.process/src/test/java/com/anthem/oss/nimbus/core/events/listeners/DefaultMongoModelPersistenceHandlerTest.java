96e152f90a817209071dbb16f3c9e7ec82f20a5d
Sandeep Mantha <AC97583@10.0.0.70>
2017-04-26 17:33:40 -0400

    query dsl support

--- platform.core.process/src/test/java/com/anthem/oss/nimbus/core/events/listeners/DefaultMongoModelPersistenceHandlerTest.java
+++ platform.core.process/src/test/java/com/anthem/oss/nimbus/core/events/listeners/DefaultMongoModelPersistenceHandlerTest.java
@@ -3,7 +3,24 @@ package com.anthem.oss.nimbus.core.events.listeners;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import javax.swing.text.DateFormatter;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,19 +28,31 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.repository.support.SpringDataMongodbQuery;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.anthem.nimbus.platform.core.process.api.AbstractPlatformIntegrationTests;
+import com.anthem.oss.nimbus.core.config.QTestDSL;
+import com.anthem.oss.nimbus.core.config.TestDSL;
 import com.anthem.oss.nimbus.core.domain.command.Command;
 import com.anthem.oss.nimbus.core.domain.command.CommandBuilder;
+import com.anthem.oss.nimbus.core.domain.command.execution.DefaultPredicateBuilder;
 import com.anthem.oss.nimbus.core.domain.model.state.EntityState.Model;
 import com.anthem.oss.nimbus.core.domain.model.state.EntityState.Param;
 import com.anthem.oss.nimbus.core.domain.model.state.QuadModel;
 import com.anthem.oss.nimbus.core.domain.model.state.builder.QuadModelBuilder;
 import com.anthem.oss.nimbus.core.domain.model.state.repo.db.DefaultMongoModelRepository;
 import com.anthem.oss.nimbus.core.entity.process.ProcessFlow;
+import com.anthem.oss.nimbus.core.entity.task.AssignmentTask;
+import com.anthem.oss.nimbus.core.entity.task.AssignmentTask.TaskType;
 import com.anthem.oss.nimbus.core.events.listeners.TestModelFlowData.Book;
 import com.anthem.oss.nimbus.core.events.listeners.TestModelFlowData.OrderBookFlow;
 import com.anthem.oss.nimbus.core.session.UserEndpointSession;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 
 /**
  * Test class to verify mongodb persistence on setState.
@@ -33,6 +62,7 @@ import com.anthem.oss.nimbus.core.session.UserEndpointSession;
  */
 
 @EnableAutoConfiguration
+@ActiveProfiles("test")
 public class DefaultMongoModelPersistenceHandlerTest  extends AbstractPlatformIntegrationTests {
 
 	@Autowired
@@ -94,5 +124,314 @@ public class DefaultMongoModelPersistenceHandlerTest  extends AbstractPlatformIn
 		assertNotNull(dbBook);
 		assertEquals(dbBook.getCategory(), "history");
 	}
+	
+	@Test 
+	public void tc02_dsl() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("testDsl");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+			    
+		QTestDSL qTest = new QTestDSL("tes");
+		Predicate predicate = qTest.name.startsWith("t");
+		SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(predicate).fetch();
+		assertEquals(list.size(), 1);
+		
+		Predicate predicate1 = qTest.name.startsWith("a");
+		SpringDataMongodbQuery<TestDSL> query1 = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list1 = query1.where(predicate1).fetch();
+		assertEquals(list1.size(), 0);
+	
+		List<TestDSL> list2 = rep._search(TestDSL.class, "testdsl", predicate);
+		assertEquals(list2.size(), 1);
+		
+		//buid predicates
+		DefaultPredicateBuilder builder = new DefaultPredicateBuilder(TestDSL.class,"tes").with("name", ":", "testDsl");
+		SpringDataMongodbQuery<TestDSL> newquery = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list3 = newquery.where(builder.build()).fetch();
+		assertEquals(list3.size(), 1);
+		
+		DefaultPredicateBuilder builder1 = new DefaultPredicateBuilder(TestDSL.class,"tes").with("name", ":", "a");
+		SpringDataMongodbQuery<TestDSL> newquery1 = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list4 = newquery1.where(builder1.build()).fetch();
+		assertEquals(list4.size(), 0);
+		
+	}
+	
+	@Test
+	public void tc03_groovy_startwith_positive() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("testDsl");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		final String groovyScript = "qTest.name.startsWith('t')";
+		final Binding binding = new Binding();
+		QTestDSL qTest = new QTestDSL("tes");
+        binding.setVariable("qTest", qTest);
 
+        final GroovyShell shell = new GroovyShell(binding); 
+        Predicate predicate = (Predicate)shell.evaluate(groovyScript); 
+        assertNotNull("Not Null", predicate);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(predicate).fetch();
+		assertEquals(list.size(), 1);
+		
+	}
+	
+	@Test
+	public void tc04_groovy_startwith_negative() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("testDsl");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		final String groovyScript = "qTest.name.startsWith('a')";
+		final Binding binding = new Binding();
+		QTestDSL qTest = new QTestDSL("te");
+        binding.setProperty("qTest", qTest);
+
+        final GroovyShell shell = new GroovyShell(binding); 
+        Predicate predicate = (Predicate)shell.evaluate(groovyScript); 
+        assertNotNull("Not Null", predicate);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(predicate).fetch();
+		assertEquals(list.size(), 0);
+		
+	}
+	
+	@Test
+	public void tc05_groovy_between_positive() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("testDsl");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		final String groovyScript = "qTest.age.between(1,11)";
+		final Binding binding = new Binding();
+		QTestDSL qTest = new QTestDSL("a");
+        binding.setProperty("qTest", qTest);
+
+        final GroovyShell shell = new GroovyShell(binding); 
+        Predicate predicate = (Predicate)shell.evaluate(groovyScript); 
+        assertNotNull("Not Null", predicate);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(predicate).fetch();
+		assertEquals(list.size(), 1);
+		
+	}
+	
+	@Test
+	public void tc06_groovy_between_negative() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("sandeep");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		final String groovyScript = "qTest.age.between(1,9)";
+		final Binding binding = new Binding();
+		QTestDSL qTest = new QTestDSL("a");
+        binding.setProperty("qTest", qTest);
+
+        final GroovyShell shell = new GroovyShell(binding); 
+        Predicate predicate = (Predicate)shell.evaluate(groovyScript); 
+        assertNotNull("Not Null", predicate);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(predicate).fetch();
+		assertEquals(list.size(), 0);
+		
+	}
+	
+	@Test
+	public void tc07_dynamicQuery_date_positive() {
+		AssignmentTask assgnmt = new AssignmentTask();
+		assgnmt.setTaskType(TaskType.patienteligibility);
+		assgnmt.setDueDate(LocalDate.now());
+		mongoOps.insert(assgnmt);
+		
+		final String groovyScript = "assignmenttask.dueDate.eq(todaydate)";
+		final Binding binding = new Binding();
+		Object obj = createQueryDslClassInstance(AssignmentTask.class);
+		
+	    assertNotNull("Not Null", obj);
+        binding.setProperty("assignmenttask", obj);
+        
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, LocalTime.MIDNIGHT);
+        localDateTime.atZone(TimeZone.getDefault().toZoneId());
+        ZoneId zoneId = TimeZone.getDefault().toZoneId();
+        
+        binding.setProperty("todaydate",localDateTime);
+        final GroovyShell shell = new GroovyShell(binding); 
+        Predicate predicate = (Predicate)shell.evaluate(groovyScript); 
+        assertNotNull("Not Null", predicate);
+        SpringDataMongodbQuery query = new SpringDataMongodbQuery(mongoOps, AssignmentTask.class);
+		List list = query.where(predicate).fetch();
+		assertEquals(list.size(), 1);
+		
+	}
+	
+	@Test
+	public void tc08_dynamicQuery_between_positive() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("sandeep");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		final String groovyScript = "qTest.age.between(1,9)";
+		final Binding binding = new Binding();
+		Object obj = createQueryDslClassInstance(TestDSL.class);
+		
+	    assertNotNull("Not Null", obj);
+        binding.setProperty("qTest", obj);
+    
+        final GroovyShell shell = new GroovyShell(binding); 
+        Predicate predicate = (Predicate)shell.evaluate(groovyScript); 
+        assertNotNull("Not Null", predicate);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(predicate).fetch();
+		assertEquals(list.size(), 0);
+		
+	}
+	
+	@Test
+	public void tc09_dynamicQuery_multipleFilter_positive() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("sandeep");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		String[] groovyScript = {"qTest.age.between(1,11)","qTest.name.startsWith('s')"};
+		final Binding binding = new Binding();
+		Object obj = createQueryDslClassInstance(TestDSL.class);
+		
+	    assertNotNull("Not Null", obj);
+        binding.setProperty("qTest", obj);
+        final GroovyShell shell = new GroovyShell(binding); 
+        List<BooleanExpression> predicates = new ArrayList<BooleanExpression>();
+        for (String string : groovyScript) {
+        	 BooleanExpression exp = (BooleanExpression)shell.evaluate(string); 
+        	 if(exp!=null) {
+        		 predicates.add(exp);
+        	 }
+		}
+        BooleanExpression result = predicates.get(0);
+        for (int i = 1; i < predicates.size(); i++) {
+            result = result.and(predicates.get(i));
+        }
+        assertNotNull("Not Null", result);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(result).fetch();
+		assertEquals(list.size(), 1);
+		
+	}
+
+	@Test
+	public void tc10_dynamicQuery_multipleFilter() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("sandeep");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		String[] groovyScript = {"qTest.age.between(1,11)","qTest.name.startsWith('a')"};
+		final Binding binding = new Binding();
+		Object obj = createQueryDslClassInstance(TestDSL.class);
+		
+	    assertNotNull("Not Null", obj);
+        binding.setProperty("qTest", obj);
+        final GroovyShell shell = new GroovyShell(binding); 
+        List<BooleanExpression> predicates = new ArrayList<BooleanExpression>();
+        for (String string : groovyScript) {
+        	 BooleanExpression exp = (BooleanExpression)shell.evaluate(string); 
+        	 if(exp!=null) {
+        		 predicates.add(exp);
+        	 }
+		}
+        BooleanExpression result = predicates.get(0);
+        for (int i = 1; i < predicates.size(); i++) {
+            result = result.and(predicates.get(i));
+        }
+        assertNotNull("Not Null", result);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(result).fetch();
+		assertEquals(list.size(), 0);
+		
+	}
+	
+	public Object createQueryDslClassInstance(Class<?> referredClass) {
+		Object obj = null;
+		try {
+			String cannonicalQuerydslclass = referredClass.getCanonicalName().replace(referredClass.getSimpleName(), "Q".concat(referredClass.getSimpleName()));
+			Class cl = Class.forName(cannonicalQuerydslclass);
+			Constructor con = cl.getConstructor(String.class);
+			obj = con.newInstance(referredClass.getSimpleName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return obj;
+	}
+	
+	@Test
+	public void tc06_groovy_and_or_postive() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("sandeep");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		final String groovyScript = "qTest.age.between(1,9).or(qTest.name.startsWith('s'))";
+		final Binding binding = new Binding();
+		QTestDSL qTest = new QTestDSL("a");
+        binding.setProperty("qTest", qTest);
+
+        final GroovyShell shell = new GroovyShell(binding); 
+        Predicate predicate = (Predicate)shell.evaluate(groovyScript); 
+        assertNotNull("Not Null", predicate);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(predicate).fetch();
+		assertEquals(list.size(), 0);
+		
+	}
+	
+	@Test
+	public void tc08_dynamicQuery_and_or_positive() {
+		TestDSL testDsl = new TestDSL();
+		testDsl.setName("sandeep");
+		testDsl.setAge(10);
+		mongoOps.insert(testDsl);
+		
+		final String groovyScript = "qTest.age.between(1,9).or(qTest.name.startsWith('s')).and(qTest.age.between(1,11))";
+		final Binding binding = new Binding();
+		Object obj = createQueryDslClassInstance(TestDSL.class);
+		
+	    assertNotNull("Not Null", obj);
+        binding.setProperty("qTest", obj);
+    
+        final GroovyShell shell = new GroovyShell(binding); 
+        Predicate predicate = (Predicate)shell.evaluate(groovyScript); 
+        assertNotNull("Not Null", predicate);
+        SpringDataMongodbQuery<TestDSL> query = new SpringDataMongodbQuery<>(mongoOps, TestDSL.class);
+		List<TestDSL> list = query.where(predicate).fetch();
+		assertEquals(list.size(), 1);
+		
+	}
 }
