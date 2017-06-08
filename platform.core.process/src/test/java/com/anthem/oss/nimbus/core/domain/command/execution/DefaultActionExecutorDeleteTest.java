ea548dfe4e542b1c6630528160edf5e07890b002
Rakesh Patel <rakeshkumar.patel@anthem.com>
2017-06-08 12:28:15 -0400

    .

--- platform.core.process/src/test/java/com/anthem/oss/nimbus/core/domain/command/execution/DefaultActionExecutorDeleteTest.java
+++ platform.core.process/src/test/java/com/anthem/oss/nimbus/core/domain/command/execution/DefaultActionExecutorDeleteTest.java
@@ -8,7 +8,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.anthem.oss.nimbus.core.AbstractUnitTest;
@@ -16,6 +18,7 @@ import com.anthem.oss.nimbus.core.domain.command.Action;
 import com.anthem.oss.nimbus.core.domain.command.Command;
 import com.anthem.oss.nimbus.core.domain.command.CommandBuilder;
 import com.anthem.oss.nimbus.core.domain.command.execution.CommandExecution.MultiOutput;
+import com.anthem.oss.nimbus.core.entity.client.Client;
 
 /**
  * @author Rakesh Patel
@@ -26,16 +29,31 @@ import com.anthem.oss.nimbus.core.domain.command.execution.CommandExecution.Mult
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DefaultActionExecutorDeleteTest extends AbstractUnitTest {
 	
+	@Autowired
+	MongoOperations mongoOps;
+	
+	
 	@Test
 	public void t0_deleteParam() {
 		
-		Command cmd = CommandBuilder.withUri("Anthem/fep/icr/p/client:5936ef35bc9e4b575f00d1a9/_delete").getCommand();
+		Client c = insertClient();
+		
+		Command cmd = CommandBuilder.withUri("Anthem/fep/icr/p/client:"+c.getId()+"/_delete").getCommand();
 		cmd.setAction(Action._delete);
 		
 		MultiOutput multiOp = getCommandGateway().execute(cmd, null);
 		Boolean b = (Boolean)multiOp.getSingleResult();
 		
 		assertTrue(b);
+	}
+	
+	private Client insertClient() {
+		
+		Client c = new Client();
+		c.setName("client1");
+		c.setCode("c1");
+		mongoOps.insert(c, "client");
+		return c;
 	}
 
 }
