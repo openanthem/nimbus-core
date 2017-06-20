0f3759f7a13dfe416f523f895b814fbde4c3655d
Vemuri, Swetha <Swetha.Vemuri@anthem.com>
2017-06-20 14:34:43 -0400

    ClientUserRole domain model changes

--- platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/entity/client/access/ClientUserRole.java
+++ platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/entity/client/access/ClientUserRole.java
@@ -7,18 +7,24 @@ import java.util.Set;
 
 import com.anthem.oss.nimbus.core.domain.definition.ConfigNature.Ignore;
 import com.anthem.oss.nimbus.core.domain.definition.Domain;
+import com.anthem.oss.nimbus.core.domain.definition.Domain.ListenerType;
+import com.anthem.oss.nimbus.core.domain.definition.Repo;
+import com.anthem.oss.nimbus.core.domain.definition.Repo.Database;
 import com.anthem.oss.nimbus.core.entity.access.Permission;
 import com.anthem.oss.nimbus.core.entity.access.Role;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * @author Soham Chakravarti
  *
  */
-@Domain(value="userrole")
-@Setter @Getter
+
+@Domain(value="userrole", includeListeners={ListenerType.persistence})
+@Repo(Database.rep_mongodb)
+@Getter @Setter @ToString(callSuper=true)
 public class ClientUserRole extends Role<ClientUserRole.Entry, ClientAccessEntity> {
 
 	private static final long serialVersionUID = 1L;
@@ -44,6 +50,11 @@ public class ClientUserRole extends Role<ClientUserRole.Entry, ClientAccessEntit
 	private Set<ClientUserRole.Entry> entries;
 	
 	//TODO figure out if this can stay as just id or needs to be converted to object reference
-	private Long clientId;
+	private String clientId;
+	
+	private String status;
+	
+	private String roleType; // standard/customized
+	
 	
 }
