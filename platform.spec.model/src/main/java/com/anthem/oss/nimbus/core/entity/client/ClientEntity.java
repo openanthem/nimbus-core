769c7970d56d816ea866cf35bf47d3868f5ee3fa
Rohit Bajaj <rohit.bajaj@anthem.com>
2017-06-20 15:00:18 -0400

    NIM-4540 Org module changes

--- platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/entity/client/ClientEntity.java
+++ platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/entity/client/ClientEntity.java
@@ -4,10 +4,14 @@
 package com.anthem.oss.nimbus.core.entity.client;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
 import com.anthem.oss.nimbus.core.domain.definition.ConfigNature.Ignore;
+import com.anthem.oss.nimbus.core.domain.definition.Domain.ListenerType;
+import com.anthem.oss.nimbus.core.domain.definition.Model;
+import com.anthem.oss.nimbus.core.domain.definition.Repo.Database;
 import com.anthem.oss.nimbus.core.domain.definition.Domain;
 import com.anthem.oss.nimbus.core.domain.definition.Repo;
 import com.anthem.oss.nimbus.core.entity.AbstractEntity;
@@ -21,8 +25,8 @@ import lombok.ToString;
  * @author Soham Chakravarti
  *
  */
-@Domain("cliententity")
-@Repo
+@Domain(value="cliententity", includeListeners={ListenerType.persistence})
+@Repo(Database.rep_mongodb)
 @Getter @Setter @ToString(callSuper=true)
 public class ClientEntity extends AbstractEntity.IdString {
 
@@ -43,7 +47,7 @@ public class ClientEntity extends AbstractEntity.IdString {
 	
 	
 	@NotNull
-	//@Model.Param.Values(url="staticCodeValue-/clientType")
+	//@Model.Param.Values(url="Anthem/icr/p/staticCodeValue/_search?fn=lookup&where=staticCodeValue.paramCode.eq('/orgType')")
     private Type type;
 	
 	private String code;
@@ -52,7 +56,7 @@ public class ClientEntity extends AbstractEntity.IdString {
 	private String name;
 	
 	@NotNull
-	//@Model.Param.Values(url="staticCodeValue-/orgStatus")
+	//@Model.Param.Values(url="Anthem/icr/p/staticCodeValue/_search?fn=lookup&where=staticCodeValue.paramCode.eq('/orgStatus')")
 	private Status status;
 	
 	private String description;
@@ -61,7 +65,9 @@ public class ClientEntity extends AbstractEntity.IdString {
 	
 	private Date terminationDate;
 
-	@Ignore private ClientEntity parentEntity;
+	//@Ignore private ClientEntity parentEntity;
+	
+	private String parentorganizationId;
 	
 	//@Ignore private Set<ClientAccessEntity> selectedAccesses;
 	
@@ -89,5 +95,4 @@ public class ClientEntity extends AbstractEntity.IdString {
 //		}
 //		getAssociatedRoles().add(cr);
 //	}
-	
 }
