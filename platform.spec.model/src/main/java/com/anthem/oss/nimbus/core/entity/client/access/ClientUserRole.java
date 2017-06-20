16e927cf0526dd3245f5eaf1c1185ff777ebfb3d
AF13233 <cheikh.niass@anthem.com>
2017-06-20 16:19:29 -0400

    added displayName and roleCategory

--- platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/entity/client/access/ClientUserRole.java
+++ platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/entity/client/access/ClientUserRole.java
@@ -3,8 +3,6 @@
  */
 package com.anthem.oss.nimbus.core.entity.client.access;
 
-import java.util.Set;
-
 import com.anthem.oss.nimbus.core.domain.definition.ConfigNature.Ignore;
 import com.anthem.oss.nimbus.core.domain.definition.Domain;
 import com.anthem.oss.nimbus.core.domain.definition.Domain.ListenerType;
@@ -12,10 +10,11 @@ import com.anthem.oss.nimbus.core.domain.definition.Repo;
 import com.anthem.oss.nimbus.core.domain.definition.Repo.Database;
 import com.anthem.oss.nimbus.core.entity.access.Permission;
 import com.anthem.oss.nimbus.core.entity.access.Role;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.Set;
 
 /**
  * @author Soham Chakravarti
@@ -28,33 +27,30 @@ import lombok.ToString;
 public class ClientUserRole extends Role<ClientUserRole.Entry, ClientAccessEntity> {
 
 	private static final long serialVersionUID = 1L;
-	
-	
-	
+    private Set<ClientUserRole.Entry> entries;
+    //TODO figure out if this can stay as just id or needs to be converted to object reference
+    private String clientId;
+    private String status;
+    private String roleType; // standard/customized
+    private String displayName;
+    private String roleCategory;
+
 	@Setter @Getter
 	public static class Entry extends Role.Entry<ClientAccessEntity> {
 
 		private static final long serialVersionUID = 1L;
-		
+
 		@Ignore
 		private ClientAccessEntity referredAccess;
-		
-		
-		public Entry() { }
-		
-		public Entry(ClientAccessEntity referredSubject, Set<Permission> grantedPermissions) {
-			super(referredSubject, grantedPermissions);
-		}
+
+
+        public Entry() {
+        }
+
+        public Entry(ClientAccessEntity referredSubject, Set<Permission> grantedPermissions) {
+            super(referredSubject, grantedPermissions);
+        }
 	}
-	
-	private Set<ClientUserRole.Entry> entries;
-	
-	//TODO figure out if this can stay as just id or needs to be converted to object reference
-	private String clientId;
-	
-	private String status;
-	
-	private String roleType; // standard/customized
 	
 	
 }
