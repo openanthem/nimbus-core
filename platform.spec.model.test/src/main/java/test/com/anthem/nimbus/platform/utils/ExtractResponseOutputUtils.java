9491f431ba47f30d88427c7d417b075a63d266b8
Vemuri, Swetha <Swetha.Vemuri@anthem.com>
2017-07-21 17:58:07 -0400

    Utils class for extract response output

---
+++ platform.spec.model.test/src/main/java/test/com/anthem/nimbus/platform/utils/ExtractResponseOutputUtils.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package test.com.anthem.nimbus.platform.utils;
+
+import com.anthem.nimbus.platform.spec.model.dsl.binder.Holder;
+import com.anthem.oss.nimbus.core.domain.command.execution.CommandExecution.MultiOutput;
+
+/**
+ * @author Swetha Vemuri
+ *
+ */
+public class ExtractResponseOutputUtils {
+
+	@SuppressWarnings("unchecked")
+	public static <T> T extractOutput(Object controllerResp) {
+		return (T)MultiOutput.class.cast(Holder.class.cast(controllerResp).getState()).getOutputs().get(0).getValue();
+	}
+	
+	public static <T> T extractOutput(Object controllerResp, int j) {
+		return extractOutput(controllerResp, 0, j);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T extractOutput(Object controllerResp, int i, int j) {
+		return (T)((MultiOutput)MultiOutput.class.cast(Holder.class.cast(controllerResp).getState()).getOutputs().get(i)).getOutputs().get(j).getValue();
+	}
+}
