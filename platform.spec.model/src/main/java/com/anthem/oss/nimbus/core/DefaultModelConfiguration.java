19e832970f25bd1e2846e08b907eee4146083056
Rakesh Patel <rakeshkumar.patel@anthem.com>
2017-08-01 12:43:26 -0400

    Added converter for code to description

--- platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/DefaultModelConfiguration.java
+++ platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/DefaultModelConfiguration.java
@@ -3,8 +3,10 @@ package com.anthem.oss.nimbus.core;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.anthem.oss.nimbus.core.domain.model.state.internal.RepoBasedCodeToDescriptionConverter;
 import com.anthem.oss.nimbus.core.domain.model.state.internal.DobToAgeConverter;
 import com.anthem.oss.nimbus.core.domain.model.state.internal.IdParamConverter;
+import com.anthem.oss.nimbus.core.domain.model.state.internal.StaticCodeValueBasedCodeToDescConverter;
 
 /**
  * @author Sandeep Mantha
@@ -22,5 +24,10 @@ public class DefaultModelConfiguration {
 	@Bean
 	public DobToAgeConverter dobToAgeConverter(){
 		return new DobToAgeConverter();
+	}
+	
+	@Bean
+	public RepoBasedCodeToDescriptionConverter codeToDescriptionConverter(){
+		return new StaticCodeValueBasedCodeToDescConverter();
 	}
 }
