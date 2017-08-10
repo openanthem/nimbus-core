2dc609192988de2ec27e53f6a5b5c3f0e89c483d
Vivek Kamineni <vivek.kamineni@anthem.com>
2017-08-10 13:54:20 -0400

    CM Problem - merge

--- platform.spec.model/src/main/java/com/anthem/nimbus/platform/spec/model/process/ProcessEngineContext.java
+++ platform.spec.model/src/main/java/com/anthem/nimbus/platform/spec/model/process/ProcessEngineContext.java
@@ -6,6 +6,7 @@ package com.anthem.nimbus.platform.spec.model.process;
 import java.io.Serializable;
 
 import com.anthem.oss.nimbus.core.domain.command.execution.ExecutionContext;
+import com.anthem.oss.nimbus.core.domain.model.state.EntityState.Model;
 import com.anthem.oss.nimbus.core.domain.model.state.EntityState.Param;
 
 import lombok.Getter;
@@ -40,5 +41,13 @@ public class ProcessEngineContext implements Serializable {
 	
 	public boolean isOutputAnException() {
 		return output != null && output instanceof Exception;
+	}
+	
+	public Object getParamValue(String paramId){
+		return executionContext.getQuadModel().getView().findParamByPath(paramId).getState();
+	}
+	
+	public Object getParamValueFromParam(Param<?> param, String paramId){
+		return param.findParamByPath(paramId).getState();
 	}
 }
