5ac9f17926cfc082d567c685215c2d6728c62f10
Soham Chakravarti <soham.chakravarti@anthem.com>
2017-10-10 11:31:12 -0400

    WIP: Added TC for Replace executor

--- platform.spec.model.test/src/main/java/com/anthem/oss/nimbus/test/sample/domain/model/core/SampleCoreLevel1_Entity.java
+++ platform.spec.model.test/src/main/java/com/anthem/oss/nimbus/test/sample/domain/model/core/SampleCoreLevel1_Entity.java
@@ -4,6 +4,8 @@
 package com.anthem.oss.nimbus.test.sample.domain.model.core;
 
 
+import java.util.List;
+
 import com.anthem.oss.nimbus.core.domain.definition.Model;
 import com.anthem.oss.nimbus.core.domain.definition.extension.ActivateConditional;
 import com.anthem.oss.nimbus.core.domain.definition.extension.Audit;
@@ -21,6 +23,8 @@ public class SampleCoreLevel1_Entity {
 	
 	@Audit(SampleCoreAuditEntry.class)
 	private String audit_nested_attr;
+	
+	private List<String> attr_list_String_noConversion;
 	
 	@ActivateConditional(when="state == 'Y'", targetPath="../level2")
 	private String level1Attrib;
