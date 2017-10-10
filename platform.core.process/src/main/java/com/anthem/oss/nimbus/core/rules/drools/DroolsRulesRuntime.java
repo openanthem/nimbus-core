b335036880577e78981d51539cbe50b2c0b282b7
Jayant Chaudhuri <jayant.chaudhuri@anthem.com>
2017-10-10 08:41:59 -0400

    Change Rules Engine Session implementation to use a stateless session. Test Case: RulesEngineTests

--- platform.core.process/src/main/java/com/anthem/oss/nimbus/core/rules/drools/DroolsRulesRuntime.java
+++ platform.core.process/src/main/java/com/anthem/oss/nimbus/core/rules/drools/DroolsRulesRuntime.java
@@ -3,12 +3,12 @@
  */
 package com.anthem.oss.nimbus.core.rules.drools;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Optional;
+import java.util.List;
 
 import org.drools.KnowledgeBase;
-import org.drools.runtime.StatefulKnowledgeSession;
-import org.drools.runtime.rule.FactHandle;
+import org.drools.runtime.StatelessKnowledgeSession;
 
 import com.anthem.oss.nimbus.core.domain.model.config.RulesConfig;
 import com.anthem.oss.nimbus.core.domain.model.state.EntityState.Param;
@@ -27,52 +27,36 @@ public class DroolsRulesRuntime implements RulesRuntime {
 
 	final private RulesConfig rulesConfig;
 
-	private StatefulKnowledgeSession session;
+	private KnowledgeBase knowledgeBase;
 	
 	@Override
 	public void start() {
-		setSession(getRulesConfig().unwrap(KnowledgeBase.class).newStatefulKnowledgeSession());
-		
-		getSession().addEventListener(new CustomRulesListener(getSession()));
+		setKnowledgeBase(getRulesConfig().unwrap(KnowledgeBase.class));
 	}
 
 	@Override
 	public void fireRules(Param<?>... params) {
-		if(getSession()==null) return;
-		
+		if(getKnowledgeBase()==null) return;
+		StatelessKnowledgeSession session = getKnowledgeBase().newStatelessKnowledgeSession();
+		List<Object> facts = new ArrayList<Object>();
 		if(params.length > 0) {
 			Arrays.asList(params).forEach((param) -> { 
-				createOrUpdateFactHandle(param);
-				Object state = param.getState();
-				createOrUpdateFactHandle(state);
+				facts.add(param);
+				facts.add(param.getState());
 			});
 		}
-		
-		getSession().fireAllRules();
+		session.execute(facts);
 	}
 
-	protected FactHandle createOrUpdateFactHandle(Object o) {
-		FactHandle factHandle = getSession().getFactHandle(o);
-		if(factHandle==null) {
-			factHandle = getSession().insert(o);	
-		} else {
-			getSession().update(factHandle, o);
-		}
-		return factHandle;
-	}
-	
 	@Override
-	public void shutdown() {
-		Optional.ofNullable(getSession())
-			.ifPresent(s->s.dispose());
-	}
+	public void shutdown() {}
 
 	@Override
 	public <S> S unwrap(Class<S> clazz) {
-		if(getSession()==null) return null;
+		if(getKnowledgeBase()==null) return null;
 		
-		if(clazz.isInstance(getSession()))
-			return clazz.cast(getSession());
+		if(clazz.isInstance(getKnowledgeBase()))
+			return clazz.cast(getKnowledgeBase());
 		
 		return null;
 	}
