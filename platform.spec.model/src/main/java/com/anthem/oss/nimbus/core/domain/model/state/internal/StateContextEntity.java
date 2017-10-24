0ced68d1ae3067a7bd9179d73d45486577d90dbe
Soham Chakravarti <soham.chakravarti@anthem.com>
2017-10-24 16:27:49 -0400

    TEMP: added string intern in ParamValue

--- platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/domain/model/state/internal/StateContextEntity.java
+++ platform.spec.model/src/main/java/com/anthem/oss/nimbus/core/domain/model/state/internal/StateContextEntity.java
@@ -3,8 +3,10 @@
  */
 package com.anthem.oss.nimbus.core.domain.model.state.internal;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.collections.CollectionUtils;
 
 import com.anthem.oss.nimbus.core.domain.definition.Domain;
 import com.anthem.oss.nimbus.core.domain.model.config.ParamValue;
@@ -35,13 +37,26 @@ public class StateContextEntity {
 	
 	private Boolean active = true;
 	
-//	@Transient
-//	private List<ParamValue> values;
+	private List<ParamValue> values;
 	
 	public List<ParamValue> getValues() {
-		return Collections.emptyList();
+		return values;
+		//return Collections.emptyList();
 	}
 	public void setValues(List<ParamValue> v) {
+		if(CollectionUtils.isEmpty(v))
+			return;
 		
+		
+		v.stream().forEach(pv->{
+			Optional.ofNullable(pv.getCode()).map(String::valueOf).map(s->s.intern())
+				.ifPresent(pv::setCode);
+			
+			Optional.ofNullable(pv.getDesc()).map(String::valueOf).map(s->s.intern())
+			.ifPresent(pv::setDesc);
+		
+			Optional.ofNullable(pv.getLabel()).map(String::valueOf).map(s->s.intern())
+			.ifPresent(pv::setLabel);
+		});
 	}
 }
