87b8bef49d8b6c579e70c5677a445788272ed51c
Antonio Lopez <AF42192@lc02sd0tng8wm.local.us.ad.wellpoint.com>
2017-11-09 16:58:18 -0600

    NIM-7627: Rule - In Progress
    Some tests are failing as a result of this change. Need to identify if they are true test failures.

---
+++ platform.core.process/src/main/java/com/anthem/oss/nimbus/core/domain/model/state/extension/RuleStateEventHandler.java
@@ -0,0 +1,93 @@
+package com.anthem.oss.nimbus.core.domain.model.state.extension;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.anthem.oss.nimbus.core.BeanResolverStrategy;
+import com.anthem.oss.nimbus.core.domain.definition.InvalidConfigException;
+import com.anthem.oss.nimbus.core.domain.definition.extension.Rule;
+import com.anthem.oss.nimbus.core.domain.model.config.RulesConfig;
+import com.anthem.oss.nimbus.core.domain.model.state.EntityState.Param;
+import com.anthem.oss.nimbus.core.domain.model.state.RulesRuntime;
+import com.anthem.oss.nimbus.core.domain.model.state.event.StateEventHandlers.OnStateChangeHandler;
+import com.anthem.oss.nimbus.core.domain.model.state.event.StateEventHandlers.OnStateLoadHandler;
+import com.anthem.oss.nimbus.core.domain.model.state.extension.AbstractConditionalStateEventHandler.EvalExprWithCrudActions;
+import com.anthem.oss.nimbus.core.rules.RulesEngineFactory;
+import com.anthem.oss.nimbus.core.util.JustLogit;
+
+/**
+ * 
+ * @author Tony Lopez (AF42192)
+ *
+ */
+public class RuleStateEventHandler extends EvalExprWithCrudActions<Rule> 
+		implements OnStateLoadHandler<Rule>, OnStateChangeHandler<Rule> {
+
+	public static final JustLogit LOG = new JustLogit();
+	
+	private static final Map<String, RulesConfig> rulesConfigCache = new HashMap<>();
+	
+	private final RulesEngineFactory rulesEngineFactory;
+	
+	public RuleStateEventHandler(BeanResolverStrategy beanResolver) {
+		super(beanResolver);
+		this.rulesEngineFactory = this.beanResolver.get(RulesEngineFactory.class);
+	}
+
+	@Override
+	protected void handleInternal(Param<?> onChangeParam, Rule configuredAnnotation) {
+		
+		for(final String ruleAlias: configuredAnnotation.value()) {
+			
+			// Build the rules runtime
+			final RulesConfig rConfig = this.getRulesConfig(ruleAlias);
+			if (null != rConfig) {
+				
+				final RulesRuntime rRuntime = this.rulesEngineFactory.createRuntime(rConfig);
+				
+				// Disallow defining @Rule with the same rule file as the root param.
+				final Param<?> rootParam = onChangeParam.getRootDomain().findParamByPath("/");
+				if (ruleAlias.equals(rootParam.getConfig().getCode())) {
+					throw new InvalidConfigException("Found [@Rule(\"" + ruleAlias + "\")] on " + 
+							onChangeParam.getParentModel().getConfig().getReferredClass() + "." + 
+							onChangeParam.getConfig().getCode() + 
+							". Rule file alias MUST differ from root param code: " + rootParam.getConfig().getCode());
+				}
+				
+				// Execute the rules.
+				this.execute(rRuntime, rootParam);
+				
+			} else {
+				
+				// Disallow a Rule annotation with a rule file alias that was not found.
+				throw new InvalidConfigException("Found [@Rule(\"" + ruleAlias + "\")] on " + 
+						onChangeParam.getParentModel().getConfig().getReferredClass() + "." + 
+						onChangeParam.getConfig().getCode() + ", but rule file was not found.");
+			}
+		}
+	}
+	
+	/**
+	 * <p>Retrieves the <tt>RulesConfig</tt> for <tt>ruleAlias</tt>. If it has already been created,
+	 * the returned value will be retrieved from a local cache relative to this StateEventHandler.</p>
+	 * 
+	 * @param ruleAlias The rule file alias
+	 * @return the <tt>RulesConfig</tt> for <tt>ruleAlias</tt>
+	 */
+	private RulesConfig getRulesConfig(String ruleAlias) {
+		return rulesConfigCache.computeIfAbsent(ruleAlias, k -> this.rulesEngineFactory.createConfig(k));
+	}
+
+	/**
+	 * Executes the rules configured for <tt>rRuntime</tt> relative to the <tt>rootParam</tt>.
+	 * 
+	 * @param rRuntime the <tt>RulesRuntime</tt> to execute.
+	 * @param rootParam the relative param from which the configured rules will execute.
+	 */
+	private void execute(RulesRuntime rRuntime, Param<?> rootParam) {			
+		rRuntime.start();
+		rRuntime.fireRules(rootParam);
+		rRuntime.shutdown();
+	}
+
+}
