6542631c6bd67027a9d0f4f1806f1799a9c59ef2
Charles Russell <charles.russell@webteks.com>
2017-12-18 13:54:42 -0500

    Cleaned up pom file, spacing, etc.  Added in needed oss sonatype snapshot deploy plugin.

--- build.sh
+++ build.sh
@@ -6,38 +6,47 @@ function installRedis {
   rm embedded-redis-0.4.jar
 }
 
+function deploySnapshots {
+ mvn -s $HOME/.m2/settings.xml -D maven.repo.local=$HOME/.m2/repository deploy -f platform.dependencies/pom.xml
+ mvn -s $HOME/.m2/settings.xml -D maven.repo.local=$HOME/.m2/repository deploy -f platform.parent/pom.xml
+ mvn -s $HOME/.m2/settings.xml -D maven.repo.local=$HOME/.m2/repository deploy -f platform.core.processor/pom.xml
+ #mvn -s $HOME/.m2/settings.xml -D maven.repo.local=$HOME/.m2/repository deploy -f platform.spec.model/pom.xml
+ #mvn -s $HOME/.m2/settings.xml -D maven.repo.local=$HOME/.m2/repository deploy -f platform.spec.model.test/pom.xml
+ mvn -s $HOME/.m2/settings.xml -D maven.repo.local=$HOME/.m2/repository deploy -f platform.web/pom.xml
+}
 
 function buildWithOutTests {
   installRedis
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -D skipDockerBuild -f platform.dependencies/pom.xml -D maven.test.skip=true
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -f platform.parent/pom.xml -D skipDockerBuild -D maven.test.skip=true
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -f platform.core.processor/pom.xml -D skipDockerBuild -D maven.test.skip=true
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -f platform.spec.model/pom.xml -D skipDockerBuild -D maven.test.skip=true
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -f platform.spec.model.test/pom.xml -D skipDockerBuild -D maven.test.skip=true
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install  -f platform.core.process/pom.xml -D skipDockerBuild -D maven.test.skip=true
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install  -f platform.web/pom.xml -P devbuild -D skipDockerBuild -D maven.test.skip=true
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -D skipDockerBuild -f platform.dependencies/pom.xml -D maven.test.skip=true
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -f platform.parent/pom.xml -D skipDockerBuild -D maven.test.skip=true
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -f platform.core.processor/pom.xml -D skipDockerBuild -D maven.test.skip=true
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -f platform.spec.model/pom.xml -D skipDockerBuild -D maven.test.skip=true
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -f platform.spec.model.test/pom.xml -D skipDockerBuild -D maven.test.skip=true
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install  -f platform.core.process/pom.xml -D skipDockerBuild -D maven.test.skip=true
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install  -f platform.web/pom.xml -P devbuild -D skipDockerBuild -D maven.test.skip=true
 }
 
 function buildWithTests {
   installRedis
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -D skipDockerBuild -f platform.dependencies/pom.xml
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -f platform.parent/pom.xml -D skipDockerBuild
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -f platform.core.processor/pom.xml -D skipDockerBuild
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -f platform.spec.model/pom.xml -D skipDockerBuild
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install -f platform.spec.model.test/pom.xml -D skipDockerBuild
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install  -f platform.core.process/pom.xml -D skipDockerBuild
-  mvn -D maven.repo.local=/Users/charles.russell/.m2/repository clean install  -f platform.web/pom.xml -P devbuild -D skipDockerBuild
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -D skipDockerBuild -f platform.dependencies/pom.xml
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -f platform.parent/pom.xml -D skipDockerBuild
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -f platform.core.processor/pom.xml -D skipDockerBuild
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -f platform.spec.model/pom.xml -D skipDockerBuild
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install -f platform.spec.model.test/pom.xml -D skipDockerBuild
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install  -f platform.core.process/pom.xml -D skipDockerBuild
+  mvn -D maven.repo.local=$HOME/.m2/repository clean install  -f platform.web/pom.xml -P devbuild -D skipDockerBuild
 }
 
 function releasePrepare {
   mvn --batch-mode -Dtag=nimbus-core-api-1.0 -Dproject.rel.com.antheminc.oss:nimbus.core.dependencies.api=1.0 -Dproject.dev.com.antheminc.oss:nimbus.core.dependencies.api=1.1-SNAPSHOT release:prepare -f platform.dependencies/pom.xml
   mvn --batch-mode -Dtag=nimbus-core-api-1.0 -Dproject.rel.com.antheminc.oss:nimbus.core.api=1.0 -Dproject.dev.com.antheminc.oss:nimbus.core.api=1.1-SNAPSHOT release:prepare -f platform.parent/pom.xml
-  #mvn -D maven.repo.local=/Users/charles.russell/.m2/repository release:clean release:prepare -f platform.parent/pom.xml -D skipDockerBuild
-  #mvn -D maven.repo.local=/Users/charles.russell/.m2/repository release:clean release:prepare -f platform.core.processor/pom.xml -D skipDockerBuild
-  #mvn -D maven.repo.local=/Users/charles.russell/.m2/repository release:clean release:prepare -f platform.spec.model/pom.xml -D skipDockerBuild
-  #mvn -D maven.repo.local=/Users/charles.russell/.m2/repository release:clean release:prepare -f platform.spec.model.test/pom.xml -D skipDockerBuild
-  #mvn -D maven.repo.local=/Users/charles.russell/.m2/repository release:clean release:prepare  -f platform.core.process/pom.xml -D skipDockerBuild
-  #mvn -D maven.repo.local=/Users/charles.russell/.m2/repository release:clean release:prepare  -f platform.web/pom.xml -D skipDockerBuild
+  #mvn -D maven.repo.local=$HOME/.m2/repository release:clean release:prepare -f platform.parent/pom.xml -D skipDockerBuild
+  #mvn -D maven.repo.local=$HOME/.m2/repository release:clean release:prepare -f platform.core.processor/pom.xml -D skipDockerBuild
+  #mvn -D maven.repo.local=$HOME/.m2/repository release:clean release:prepare -f platform.spec.model/pom.xml -D skipDockerBuild
+  #mvn -D maven.repo.local=$HOME/.m2/repository release:clean release:prepare -f platform.spec.model.test/pom.xml -D skipDockerBuild
+  #mvn -D maven.repo.local=$HOME/.m2/repository release:clean release:prepare  -f platform.core.process/pom.xml -D skipDockerBuild
+  #mvn -D maven.repo.local=$HOME/.m2/repository release:clean release:prepare  -f platform.web/pom.xml -D skipDockerBuild
 }
-releasePrepare
+#releasePrepare
 #buildWithOutTests
+deploySnapshots
