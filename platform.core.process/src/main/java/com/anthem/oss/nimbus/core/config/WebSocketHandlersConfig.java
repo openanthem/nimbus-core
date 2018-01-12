c3f0e884c657654400e727499675e90b47ba15c2
Vemuri, Swetha <Swetha.Vemuri@anthem.com>
2018-01-12 12:41:12 -0500

    1.Generate license header after year change 2.Remove empty file comments

--- platform.core.process/src/main/java/com/anthem/oss/nimbus/core/config/WebSocketHandlersConfig.java
+++ platform.core.process/src/main/java/com/anthem/oss/nimbus/core/config/WebSocketHandlersConfig.java
@@ -1,6 +1,5 @@
 /**
- *
- *  Copyright 2012-2017 the original author or authors.
+ *  Copyright 2016-2018 the original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package com.anthem.oss.nimbus.core.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
--- platform.spec.model.test/src/main/java/com/anthem/oss/nimbus/test/sample/domain/model/ui/VPSampleViewPageGreen.java
+++ platform.spec.model.test/src/main/java/com/anthem/oss/nimbus/test/sample/domain/model/ui/VPSampleViewPageGreen.java
@@ -1,6 +1,5 @@
 /**
- *
- *  Copyright 2012-2017 the original author or authors.
+ *  Copyright 2016-2018 the original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,9 +12,6 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- */
-/**
- * 
  */
 package com.anthem.oss.nimbus.test.sample.domain.model.ui;
 
