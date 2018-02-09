dec132b7500fc45720e24e4626ae139ee949c43c
Meda, Dinakar <dinakar.meda@empireblue.com>
2018-02-09 17:05:26 -0500

    update license

--- nimbus-ui/nimbusui/src/app/components/platform/section.component.ts
+++ nimbus-ui/nimbusui/src/app/components/platform/section.component.ts
@@ -1,10 +1,21 @@
 /**
  * @license
- * Copyright Anthem Inc. All Rights Reserved.
- *
- * This source code is released under version 2.0 of the Apache License.
- * The LICENSE information can be found at http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2016-2018 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+'use strict';
+
 import { Component, Input, OnInit } from '@angular/core';
 import { Param } from '../../shared/app-config.interface';
 import { PageService } from '../../services/page.service';
