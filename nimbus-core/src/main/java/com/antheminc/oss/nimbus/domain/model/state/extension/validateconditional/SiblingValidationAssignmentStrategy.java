c8526c1be203b5431dc393909e84f1a535f3a29e
Antonio Lopez <AF42192@lc02sd0tng8wm.local.us.ad.wellpoint.com>
2018-02-16 15:26:41 -0600

    NIM-10167: @ValidateConditional - Code Cleanup

--- nimbus-core/src/main/java/com/antheminc/oss/nimbus/domain/model/state/extension/validateconditional/SiblingValidationAssignmentStrategy.java
+++ nimbus-core/src/main/java/com/antheminc/oss/nimbus/domain/model/state/extension/validateconditional/SiblingValidationAssignmentStrategy.java
@@ -4,7 +4,7 @@ import com.antheminc.oss.nimbus.domain.defn.extension.ValidateConditional.Valida
 import com.antheminc.oss.nimbus.domain.model.state.EntityState.Param;
 
 /**
- * <p>This strategy will first find all siblings of <tt>param</tt>, inclusively. For each 
+ * <p>This strategy will first find all siblings of <tt>onChangeParam</tt>, inclusively. For each 
  * sibling that has a conditional <tt>Constraint</tt> that should be applied, this strategy will 
  * attempt to add the identified "group" of that <tt>Constraint</tt> to the respective param's 
  * <tt>activeValidationGroups</tt>.</p>
@@ -20,13 +20,13 @@ import com.antheminc.oss.nimbus.domain.model.state.EntityState.Param;
 public final class SiblingValidationAssignmentStrategy extends AbstractValidationAssignmentStrategy {
 
 	@Override
-	void assignGroupTo(Param<?> param, Class<? extends ValidationGroup> group) {
-		this.handleSiblings(param, group, this::addGroupToParam);
+	void assignGroupTo(Param<?> onChangeParam, Class<? extends ValidationGroup> targetGroup) {
+		this.handleSiblings(onChangeParam, targetGroup, this::addGroupToParam);
 	}
 
 	@Override
-	void unassignGroupFrom(Param<?> param, Class<? extends ValidationGroup> group) {
-		this.handleSiblings(param, group, this::removeGroupFromParam);
+	void unassignGroupFrom(Param<?> onChangeParam, Class<? extends ValidationGroup> targetGroup) {
+		this.handleSiblings(onChangeParam, targetGroup, this::removeGroupFromParam);
 	}
 
 }
