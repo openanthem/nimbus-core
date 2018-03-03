7d01d162747e1be4b7f7430d439345d859919751
Antonio Lopez <AF42192@lc02sd0tng8wm.local.us.ad.wellpoint.com>
2018-03-03 00:44:01 -0600

    Discourse 143 - Test Case Added per Request
    http://discourse.oss.antheminc.com/t/configuring-a-transient-modal-form-not-working-with-conditional-annotations/143

---
+++ nimbus-test/src/main/java/com/antheminc/oss/nimbus/test/scenarios/s4/view/S4Values.java
@@ -0,0 +1,70 @@
+/**
+ *  Copyright 2016-2018 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.antheminc.oss.nimbus.test.scenarios.s4.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.antheminc.oss.nimbus.domain.defn.Model.Param.Values.Source;
+import com.antheminc.oss.nimbus.domain.model.config.ParamValue;
+
+/**
+ * @author Tony Lopez
+ *
+ */
+public class S4Values {
+
+	public static class Q1_Preferences implements Source {
+		/* (non-Javadoc)
+		 * @see com.antheminc.oss.nimbus.domain.defn.Model.Param.Values.Source#getValues(java.lang.String)
+		 */
+		@Override
+		public List<ParamValue> getValues(String paramCode) {
+			List<ParamValue> values = new ArrayList<>();
+			values.add(new ParamValue("preference1", "Preference 1"));
+			values.add(new ParamValue("preference2", "Preference 2"));
+			return values;
+		}
+	}
+	
+	public static class Preference1Types implements Source {
+		/* (non-Javadoc)
+		 * @see com.antheminc.oss.nimbus.domain.defn.Model.Param.Values.Source#getValues(java.lang.String)
+		 */
+		@Override
+		public List<ParamValue> getValues(String paramCode) {
+			List<ParamValue> values = new ArrayList<>();
+			values.add(new ParamValue("preference1a", "Preference 1A"));
+			values.add(new ParamValue("preference1b", "Preference 1B"));
+			values.add(new ParamValue("preference1c", "Preference 1C"));
+			return values;
+		}
+	}
+	
+	public static class Preference2Types implements Source {
+		/* (non-Javadoc)
+		 * @see com.antheminc.oss.nimbus.domain.defn.Model.Param.Values.Source#getValues(java.lang.String)
+		 */
+		@Override
+		public List<ParamValue> getValues(String paramCode) {
+			List<ParamValue> values = new ArrayList<>();
+			values.add(new ParamValue("preference2a", "Preference 2A"));
+			values.add(new ParamValue("preference2b", "Preference 2B"));
+			values.add(new ParamValue("preference2c", "Preference 2C"));
+			return values;
+		}
+	}
+}
