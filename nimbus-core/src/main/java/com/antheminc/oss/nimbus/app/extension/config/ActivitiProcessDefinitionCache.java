e0f93bd9010fbf03758c14ebf820772a0228f645
Rakesh Patel <rakeshkumar.patel@anthem.com>
2018-03-14 22:46:20 -0400

    Fixes NIM-10947 - activiti process definition caching

--- nimbus-core/src/main/java/com/antheminc/oss/nimbus/app/extension/config/ActivitiProcessDefinitionCache.java
+++ nimbus-core/src/main/java/com/antheminc/oss/nimbus/app/extension/config/ActivitiProcessDefinitionCache.java
@@ -1,5 +1,17 @@
 /**
- * 
+ *  Copyright 2016-2018 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.antheminc.oss.nimbus.app.extension.config;
 
@@ -12,15 +24,15 @@ import org.activiti.engine.impl.persistence.deploy.ProcessDefinitionCacheEntry;
  */
 public class ActivitiProcessDefinitionCache extends DefaultDeploymentCache<ProcessDefinitionCacheEntry> {
 	
-  public ProcessDefinitionCacheEntry find(String id) {
-	  String processName = id.split(":")[0];
-	  for(String key:cache.keySet()) {
-		  String processDefinition = key.split(":")[0];
-		  if(processDefinition.equals(processName)) {
-			  return cache.get(key);
+	  public ProcessDefinitionCacheEntry findByKey(String currentKey) {
+		  for(String processDefinition:cache.keySet()) {
+			  String key = processDefinition.split(":")[0];
+			  if(key.equals(currentKey)) {
+				  ProcessDefinitionCacheEntry entry = cache.get(processDefinition);
+				  return entry;
+			  }
 		  }
+		  return null;
 	  }
-	  return null;
-  }
 
 }
