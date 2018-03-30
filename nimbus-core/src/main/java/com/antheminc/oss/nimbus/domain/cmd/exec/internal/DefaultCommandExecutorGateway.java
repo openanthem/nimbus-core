169f885833d44851918817e209bf609ade1f2a3d
Soham Chakravarti <soham.chakravarti@anthem.com>
2018-03-30 16:02:05 -0400

    added sequence to multioutput

--- nimbus-core/src/main/java/com/antheminc/oss/nimbus/domain/cmd/exec/internal/DefaultCommandExecutorGateway.java
+++ nimbus-core/src/main/java/com/antheminc/oss/nimbus/domain/cmd/exec/internal/DefaultCommandExecutorGateway.java
@@ -19,6 +19,7 @@ import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -150,7 +151,7 @@ public class DefaultCommandExecutorGateway extends BaseCommandExecutorStrategies
 	}
 
 	private MultiOutput createFlattenedOutput(MultiOutput in) {
-		Map<Object, Output<?>> uniqueValues = new HashMap<>();
+		Map<Object, Output<?>> uniqueValues = new LinkedHashMap<>();
 		
 		flattenOutput(in, uniqueValues);
 		
