be964696248490d4592b1609b4ebfb8fd321b975
Purna Chander Mashetty <AF73944@LC02VW053HTDD>
2018-04-03 15:02:50 -0400

    created UI testcases

---
+++ nimbus-ui/nimbusui/src/app/components/platform/form/elements/textbox.component.spec.ts
@@ -0,0 +1,41 @@
+'use strict';
+import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpModule } from '@angular/http';
+
+import { InputText } from './textbox.component';
+import { TooltipComponent } from '../../tooltip/tooltip.component'
+import { PageService } from '../../../../services/page.service';
+import { CustomHttpClient } from '../../../../services/httpclient.service';
+import { LoaderService } from '../../../../services/loader.service';
+import { ConfigService } from '../../../../services/config.service';
+
+describe('InputText', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        InputText,
+        TooltipComponent
+       ],
+       imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        HttpModule
+       ],
+       providers: [
+        PageService,
+        CustomHttpClient,
+        LoaderService,
+        ConfigService
+       ]
+    }).compileComponents();
+  }));
+
+  it('InputText should create the app', async(() => {
+    const fixture = TestBed.createComponent(InputText);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+});
