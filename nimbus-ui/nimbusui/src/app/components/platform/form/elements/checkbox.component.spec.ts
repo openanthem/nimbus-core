b71a66b2f6d6497b2376cde0f226af6ea42979d0
Purna Chander Mashetty <AF73944@LC02VW053HTDD>
2018-04-10 16:07:05 -0400

    Created phase2 UI testcases

---
+++ nimbus-ui/nimbusui/src/app/components/platform/form/elements/checkbox.component.spec.ts
@@ -0,0 +1,41 @@
+'use strict';
+import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
+import { CheckBox } from './checkbox.component';
+import { TooltipComponent } from '../../../platform/tooltip/tooltip.component';
+import { PageService } from '../../../../services/page.service';
+import { CustomHttpClient } from '../../../../services/httpclient.service';
+import { LoaderService } from '../../../../services/loader.service';
+import { ConfigService } from '../../../../services/config.service';
+
+describe('CheckBox', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CheckBox,
+        TooltipComponent
+       ],
+       imports: [
+        FormsModule,
+        HttpClientModule,
+        HttpModule
+       ],
+       providers:[
+        PageService,
+        CustomHttpClient,
+        LoaderService,
+        ConfigService
+       ]
+    }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(CheckBox);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+});
