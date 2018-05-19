71f266e0edaa4f1968bba605fb05efca6a4f6478
Rakesh Patel <rakeshkumar.patel@anthem.com>
2018-05-19 00:19:28 -0400

    test cases for changelog

---
+++ nimbus-test/src/main/java/com/antheminc/oss/nimbus/test/scenarios/changelog/view/VRSampleCoreChangLogEntityA.java
@@ -0,0 +1,81 @@
+package com.antheminc.oss.nimbus.test.scenarios.changelog.view;
+
+import com.antheminc.oss.nimbus.domain.defn.Domain;
+import com.antheminc.oss.nimbus.domain.defn.Domain.ListenerType;
+import com.antheminc.oss.nimbus.domain.defn.Execution.Config;
+import com.antheminc.oss.nimbus.domain.defn.Executions.Configs;
+import com.antheminc.oss.nimbus.domain.defn.MapsTo;
+import com.antheminc.oss.nimbus.domain.defn.MapsTo.Path;
+import com.antheminc.oss.nimbus.domain.defn.Model;
+import com.antheminc.oss.nimbus.domain.defn.Repo;
+import com.antheminc.oss.nimbus.domain.defn.Repo.Database;
+import com.antheminc.oss.nimbus.domain.defn.ViewConfig.Button;
+import com.antheminc.oss.nimbus.domain.defn.ViewConfig.Form;
+import com.antheminc.oss.nimbus.domain.defn.ViewConfig.Page;
+import com.antheminc.oss.nimbus.domain.defn.ViewConfig.Section;
+import com.antheminc.oss.nimbus.domain.defn.ViewConfig.Tile;
+import com.antheminc.oss.nimbus.test.scenarios.changelog.core.SampleCoreChangeLogEntityA;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Rakesh Patel
+ *
+ */
+@Domain(value="samplechangelogview_a", includeListeners={ListenerType.websocket})
+@MapsTo.Type(SampleCoreChangeLogEntityA.class)
+@Repo(Database.rep_none)
+@Getter @Setter
+public class VRSampleCoreChangLogEntityA {
+	
+	@Page
+	private VPSampleCoreChangeLog vpSampleCoreChangeLog;
+	
+	
+	@Model
+	@Getter @Setter
+	public static class VPSampleCoreChangeLog {
+		
+		@Tile
+		private VTSampleCoreChangeLog vtSampleCoreChangeLog;
+	}
+	
+	
+	@Model
+	@Getter @Setter
+	public static class VTSampleCoreChangeLog {
+		
+		@Section
+		private VSSampleCoreChangeLog vsSampleCoreChangeLog;
+	}
+	
+	
+	@Model
+	@Getter @Setter
+	public static class VSSampleCoreChangeLog {
+	
+		@Form
+		private VFSampleCoreChangeLog vfSampleCoreChangeLog;
+	}
+	
+	
+	@MapsTo.Type(SampleCoreChangeLogEntityA.class)
+	@Getter @Setter
+	public static class VFSampleCoreChangeLog {
+		
+		@Path
+		private String status;
+		
+		@Path
+		private String statusSetViaRule;
+		
+		@Button
+		@Configs({
+			@Config(url="/.d/.m/sampleCoreChangLogEntityNested/nestedStatus/_update?rawPayload=\"Test_Nested_Status\""),
+			@Config(url="/p/samplechangelogview_b/_new?fn=_initEntity&target=/.m/status&json=\"Test_Status_B\""),
+		})
+		private String button;
+		
+	}
+}
---
+++ nimbus-test/src/test/java/com/antheminc/oss/nimbus/test/scenarios/changelog/ChangeLogTest.java
@@ -0,0 +1,107 @@
+package com.antheminc.oss.nimbus.test.scenarios.changelog;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import com.antheminc.oss.nimbus.domain.cmd.Action;
+import com.antheminc.oss.nimbus.domain.model.state.EntityState.Param;
+import com.antheminc.oss.nimbus.domain.model.state.ModelEvent;
+import com.antheminc.oss.nimbus.domain.model.state.extension.ChangeLogCommandEventHandler.ChangeLogEntry;
+import com.antheminc.oss.nimbus.test.domain.support.AbstractFrameworkIntegrationTests;
+import com.antheminc.oss.nimbus.test.domain.support.utils.ExtractResponseOutputUtils;
+import com.antheminc.oss.nimbus.test.domain.support.utils.MockHttpRequestBuilder;
+
+/**
+ * @author Rakesh Patel
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ChangeLogTest extends AbstractFrameworkIntegrationTests {
+
+	private static final String CL_VIEW_A = PLATFORM_ROOT + "/samplechangelogview_a";
+	
+	private static Long CL_VIEW_A_ID;
+	
+	@Test
+	public void t00_create_entity_A() throws Exception {
+		HttpServletRequest httpReq = MockHttpRequestBuilder.withUri(CL_VIEW_A)
+			.addAction(Action._new)
+			.getMock();
+		
+		Object controllerResp = controller.handleGet(httpReq, null);
+		assertThat(controllerResp).isNotNull();
+		
+		Param<?> p = ExtractResponseOutputUtils.extractOutput(controllerResp);
+		assertThat(p).isNotNull();
+		
+		CL_VIEW_A_ID = p.findStateByPath("/.m/id");
+		assertThat(CL_VIEW_A_ID).isNotNull();
+		
+		List<ChangeLogEntry> entries = mongo.find(new Query(Criteria.where("url").regex("samplechangelogview_a/_new")), ChangeLogEntry.class, "changelog");
+		assertThat(entries).isNotEmpty();
+		
+	}
+	
+	@Test
+	public void t01_update_EventNotifyOnNestedParam_entity_A() throws Exception {
+		HttpServletRequest httpReq = MockHttpRequestBuilder.withUri(PLATFORM_ROOT)
+				.addNested("/event/notify")
+				.getMock();
+		
+		ModelEvent<String> modelEvent_q1 = new ModelEvent<>();
+		modelEvent_q1.setId("/samplechangelogview_a:"+CL_VIEW_A_ID+"/vpSampleCoreChangeLog/vtSampleCoreChangeLog/vsSampleCoreChangeLog/vfSampleCoreChangeLog/status/_update");
+		modelEvent_q1.setType(Action._update.name());
+		modelEvent_q1.setPayload(json.write("Test_Status").getJson());
+		
+		Object controllerResp = controller.handleEventNotify(httpReq, modelEvent_q1);
+		assertThat(controllerResp).isNotNull();
+		
+		ChangeLogEntry cmdEntry = mongo.findOne(new Query(Criteria.where("url").regex("vpSampleCoreChangeLog/vtSampleCoreChangeLog/vsSampleCoreChangeLog/vfSampleCoreChangeLog/status")), ChangeLogEntry.class, "changelog");
+		assertThat(cmdEntry).isNotNull();
+		
+		ChangeLogEntry valueEntry = mongo.findOne(new Query(Criteria.where("value").is("Test_Status")), ChangeLogEntry.class, "changelog");
+		assertThat(valueEntry).isNotNull();
+		
+		ChangeLogEntry valueEntryViaRule = mongo.findOne(new Query(Criteria.where("value").is("Test_Status_Via_Rule")), ChangeLogEntry.class, "changelog");
+		assertThat(valueEntryViaRule).isNotNull();
+		
+	}
+	
+	@Test
+	public void t02_execute_ParamWithConfigs_entity_A() throws Exception {
+		HttpServletRequest httpReq = MockHttpRequestBuilder.withUri(CL_VIEW_A)
+				.addRefId(CL_VIEW_A_ID)
+				.addNested("/vpSampleCoreChangeLog/vtSampleCoreChangeLog/vsSampleCoreChangeLog/vfSampleCoreChangeLog/button")
+				.addAction(Action._get)
+				.getMock();
+		
+		Object controllerResp = controller.handleGet(httpReq, null);
+		assertThat(controllerResp).isNotNull();
+		
+		ChangeLogEntry cmdEntry = mongo.findOne(new Query(Criteria.where("url").regex("vpSampleCoreChangeLog/vtSampleCoreChangeLog/vsSampleCoreChangeLog/vfSampleCoreChangeLog/button")), ChangeLogEntry.class, "changelog");
+		assertThat(cmdEntry).isNotNull();
+		
+		ChangeLogEntry subCmdEntryWithRootOnlyPath = mongo.findOne(new Query(Criteria.where("url").regex("samplechangelogview_b/_new")), ChangeLogEntry.class, "changelog");
+		assertThat(subCmdEntryWithRootOnlyPath).isNotNull();
+		
+		ChangeLogEntry subCmdEntryWithNestedPath = mongo.findOne(new Query(Criteria.where("url").regex("sampleCoreChangLogEntityNested/nestedStatus/_update")), ChangeLogEntry.class, "changelog");
+		assertThat(subCmdEntryWithNestedPath).isNotNull();
+		
+		ChangeLogEntry valueEntry_NestedEntityA = mongo.findOne(new Query(Criteria.where("value").is("Test_Nested_Status")), ChangeLogEntry.class, "changelog");
+		assertThat(valueEntry_NestedEntityA).isNotNull();
+		
+		ChangeLogEntry valueEntry_EntityB = mongo.findOne(new Query(Criteria.where("value").is("Test_Status_B")), ChangeLogEntry.class, "changelog");
+		assertThat(valueEntry_EntityB).isNotNull(); 
+		
+	}
+	
+}
