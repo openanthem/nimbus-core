55b149683a534c8cba7e910fedf182ab2b56e493
Antonio Lopez <AF42192@lc02sd0tng8wm.local.us.ad.wellpoint.com>
2018-08-13 15:58:21 -0500

    NIM-17256: Signature component behaves inconsistently

--- nimbus-core/src/main/java/com/antheminc/oss/nimbus/domain/defn/ViewConfig.java
+++ nimbus-core/src/main/java/com/antheminc/oss/nimbus/domain/defn/ViewConfig.java
@@ -1403,9 +1403,6 @@ public class ViewConfig {
 	 * <p><b>Expected Field Structure</b>
 	 * 
 	 * <p>Radio will be rendered when annotating a field nested under one of the following components:
-	 * <ul>
-	 * <li>{@link Form}</li>
-	 * </ul>
 	 * 
 	 * <p>Radio should decorate a field having a simple type.
 	 * 
@@ -1472,6 +1469,9 @@ public class ViewConfig {
 	}
 	
 	/**
+	 * <p>The Signature component is used to capture a user's signature using user input in the form of
+	 * a <a href="https://www.ietf.org/rfc/rfc2397.txt">Data URL</a>.
+	 * 
 	 * <p><b>Expected Field Structure</b>
 	 * 
 	 * <p>Signature will be rendered when annotating a field nested under one of the following components:
@@ -1480,6 +1480,11 @@ public class ViewConfig {
 	 * </ul>
 	 * 
 	 * <p>Signature should decorate a field having a simple type.
+	 * <p><b>Sample Usage:</b>
+	 * <pre>
+	 * &#64;Signature
+	 * private String userSignature;
+	 * </pre>
 	 * 
 	 * @since 1.0
 	 * @author Rakesh Patel
@@ -1488,44 +1493,72 @@ public class ViewConfig {
 	@Retention(RetentionPolicy.RUNTIME) 
 	@Target({ElementType.FIELD}) 
 	@ViewStyle 
-	public @interface Signature { 
+	public @interface Signature {
 		
 		/**
-		 * The strategy for how the signature drawing should be captured on the UI.
+		 * <p>The strategy for how the signature drawing should be captured on the UI.
 		 */
 		public enum CaptureType {
 			
 			/**
-			 * Signature data is captured in between the mouse down and mouse up events.
+			 * <p>Signature data is captured in between the mouse down and mouse up events.
 			 */
 			DEFAULT,
 			
 			/**
-			 * Signature data is captured upon the click event. Capturing will continue until the click 
+			 * <p>Signature data is captured upon the click event. Capturing will continue until the click 
 			 * event is invoked a second time.
 			 */
 			ON_CLICK;
 		}
 		
+		/**
+		 * <p>The label value displayed on the "save" button.
+		 */
 		String acceptLabel() default "Save";
+		/**
+		 * <p>To be used by the client as a unique identifier for this component.
+		 * <p><b>THIS VALUE SHOULD NOT BE CHANGED!
+		 */
 		String alias() default "Signature"; 
 		/**
-		 * Controls how the signature drawing will be captured.
+		 * <p>Control how the signature drawing will be captured.
 		 * @see com.antheminc.oss.nimbus.domain.defn.ViewConfig.Signature.CaptureType
 		 */
-		CaptureType captureType() default CaptureType.DEFAULT; 
-		String clearLabel() default "Clear"; 
-		String controlId() default "";
-		String height() default "60"; 
-		String help() default "";
-		boolean hidden() default false;
-		String labelClass() default "anthem-label";
-		boolean postEventOnChange() default false;
-		String type() default "signature";
+		CaptureType captureType() default CaptureType.DEFAULT;
+		/**
+		 * <p>The label value displayed on the "clear" button.
+		 */
+		String clearLabel() default "Clear";
+		/**
+		 * <p>CSS classes added here will be added to the container element surrounding this component.
+		 * <p>This can be used to apply additional styling, if necessary.
+		 */
 		String cssClass() default "";
+		/**
+		 * <p>The width of the signature canvas. 
+		 */
+		String height() default "60";
+		/**
+		 * <p>When {@code true}, the the label and help text will be hidden for this component.
+		 */
+		boolean hidden() default false;
+		/**
+		 * <p>When {@code true} and the value of this component is changed on the client, the updated 
+		 * value will be sent to the server.
+		 */
+		boolean postEventOnChange() default false;
+		/**
+		 * <p>To be used by the client as a unique identifier for this component.
+		 * <p><b>THIS VALUE SHOULD NOT BE CHANGED!
+		 */
+		String type() default "signature";
+		/**
+		 * <p>The width of the signature canvas. 
+		 */
 		String width() default "345";
 	}
-	
+
 	/**
 	 * <p><b>Expected Field Structure</b>
 	 * 
