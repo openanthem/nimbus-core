d5b9aba5f1fcb06bc344b8a6d9d264b351a3e432
Vivek Kamineni <vivek.kamineni@anthem.com>
2018-08-29 11:39:54 -0400

    stackoverflow test

--- nimbus-test/src/test/java/com/antheminc/oss/nimbus/test/scenarios/s10/S10_StackoverflowTest.java
+++ nimbus-test/src/test/java/com/antheminc/oss/nimbus/test/scenarios/s10/S10_StackoverflowTest.java
@@ -1,38 +1,46 @@
 package com.antheminc.oss.nimbus.test.scenarios.s10;
 
+import java.io.StringWriter;
+import java.io.Writer;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.antheminc.oss.nimbus.domain.cmd.Action;
 import com.antheminc.oss.nimbus.test.domain.support.AbstractFrameworkIntegrationTests;
 import com.antheminc.oss.nimbus.test.domain.support.utils.ExtractResponseOutputUtils;
 import com.antheminc.oss.nimbus.test.domain.support.utils.MockHttpRequestBuilder;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
-public class S10_StackoverflowTest extends AbstractFrameworkIntegrationTests{
-	  @Autowired
-	    public ObjectMapper mapper;
-	  
-//	  @Autowired
-//	    public ObjectWriter mapper2;
-	
+public class S10_StackoverflowTest extends AbstractFrameworkIntegrationTests {
+
+	@Autowired
+	public ObjectMapper mapper;
+
 	@Test
 	public void t00_init() throws Exception {
+
+		Writer jsonWriter = new StringWriter();
+		JsonFactory jsonFactory = new JsonFactory();
+		jsonFactory.setCodec(mapper);
+		JsonGenerator jsonGenerator = jsonFactory.createGenerator(jsonWriter);
+
+		Object controllerResp_new = controller.handleGet(
+				MockHttpRequestBuilder.withUri("/client/org/app/p/s10_view").addAction(Action._new).getMock(), null);
+
+//       This also throws stackoverflow error.
+//		 mapper.writeValueAsString(controllerResp_new);
+
 		
-		
-		
-		Object controllerResp_new = controller.handleGet(MockHttpRequestBuilder.withUri("/client/org/app/p/s10_view")
-				.addAction(Action._new).getMock(), null);
-		
-		mapper.writeValueAsString(controllerResp_new);
-		
-//		mapper2.writeValue(generator, controllerResp_new);
-		
-		
-		
-//		controllerResp_new  s10_view
-//		Assert.assertNotNull(ExtractResponseOutputUtils.extractOutput(controllerResp_new));
+		ObjectWriter objectWriter = mapper.writerWithDefaultPrettyPrinter();
+		objectWriter.writeValue(jsonGenerator, controllerResp_new);
+
 	}
 }
