b2f51e6a9f2341ab65772ccca329820111157b7c
Vivek Kamineni <vivek.kamineni@anthem.com>
2018-09-10 15:01:00 -0400

    License Info

--- nimbus-ui/nimbusui/src/app/components/platform/tree-grid/tree-grid.component.ts
+++ nimbus-ui/nimbusui/src/app/components/platform/tree-grid/tree-grid.component.ts
@@ -1,5 +1,22 @@
+/**
+ * @license
+ * Copyright 2016-2018 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 import { Component, Input } from '@angular/core';
-import { TreeNode } from 'primeng/api';
 import { FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ControlValueAccessor } from '@angular/forms/src/directives';
 import { BaseElement } from './../base-element.component';
@@ -7,6 +24,12 @@ import { WebContentSvc } from '../../../services/content-management.service';
 import { ParamConfig } from '../../../shared/param-config';
 import { PageService } from '../../../services/page.service';
 import { GenericDomain } from '../../../model/generic-domain.model';
+
+
+/**
+ * \@author Vivek Kamineni
+ * \@whatItDoes A control to be used to display closely related data in a Tree structure.
+ */
 
 
 @Component({
