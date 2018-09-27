2d9d35721318a8a64a5a46fd7f0b6be60d17df7f
Vivek Kamineni <vivek.kamineni@anthem.com>
2018-09-27 14:30:03 -0400

    pick list not working within form

--- nimbus-ui/nimbusui/src/app/components/platform/form-group.component.ts
+++ nimbus-ui/nimbusui/src/app/components/platform/form-group.component.ts
@@ -54,6 +54,10 @@ export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
             <ng-template [ngIf]="!element.type?.model?.params?.length || element.config?.type?.collection">
                 <nm-element [position]="position+1" id="{{id}}" [element]="element" [elementCss]="elementCss" [form]="form"></nm-element>
             </ng-template>
+
+            <ng-template [ngIf]="element?.config?.uiStyles?.attributes?.alias === viewComponent.picklist.toString()">
+               <nm-element [position]="position+1" id="{{id}}" [element]="element" [elementCss]="elementCss" [form]="form"></nm-element>
+            </ng-template>
             
             <ng-template [ngIf]="element.config?.uiStyles?.attributes?.alias == viewComponent.button.toString()">
                 <nm-button [form]="form" [element]="element"> </nm-button>
