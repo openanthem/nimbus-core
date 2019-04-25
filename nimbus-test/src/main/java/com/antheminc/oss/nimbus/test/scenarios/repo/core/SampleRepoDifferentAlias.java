04c5e32a289a9307fb266f7f85ff0c0ec2c4cf05
Tony Lopez <antoniotony.lopez@anthem.com>
2019-04-25 10:53:20 -0500

    NIMBUS-182: repo alias persistence fix

---
+++ nimbus-test/src/main/java/com/antheminc/oss/nimbus/test/scenarios/repo/core/SampleRepoDifferentAlias.java
@@ -0,0 +1,45 @@
+/**
+ *  Copyright 2016-2019 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.antheminc.oss.nimbus.test.scenarios.repo.core;
+
+import com.antheminc.oss.nimbus.domain.defn.Domain;
+import com.antheminc.oss.nimbus.domain.defn.Domain.ListenerType;
+import com.antheminc.oss.nimbus.domain.defn.Repo;
+import com.antheminc.oss.nimbus.domain.defn.Repo.Database;
+import com.antheminc.oss.nimbus.entity.AbstractEntity.IdLong;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Tony Lopez
+ *
+ */
+@Domain(value="sample_repo_diff_alias", includeListeners={ListenerType.persistence})
+@Repo(alias = "person_of_interest", value = Database.rep_mongodb)
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class SampleRepoDifferentAlias extends IdLong {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String firstName;
+	private String lastName;
+}
