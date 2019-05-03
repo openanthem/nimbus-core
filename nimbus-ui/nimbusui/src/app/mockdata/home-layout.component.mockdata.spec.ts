cba414ee1bdfd3666a4d0c70649e5b4ec1ea500a
Tony Lopez <antoniotony.lopez@anthem.com>
2019-05-03 10:57:08 -0500

    text fixes

--- nimbus-ui/nimbusui/src/app/mockdata/home-layout.component.mockdata.spec.ts
+++ nimbus-ui/nimbusui/src/app/mockdata/home-layout.component.mockdata.spec.ts
@@ -16,73 +16,77 @@
  */
 
 export const Homelayout:any = {
-    "menu": [
-        {
-            "label": "Home",
-            "path": "/home/vpHome/vsHomeLeftBar/home",
-            "page": "",
-            "icon": "tasksIcon",
-            "imgType": "FA",
-            "url": "petclinicdashboard/vpDashboard",
-            "type": "INTERNAL",
-            "target": "",
-            "rel": "",
-            "routerLink": "/h/petclinicdashboard/vpDashboard",
-            "code": "home"
-        },
-        {
-            "label": "Veterinarians",
-            "path": "/home/vpHome/vsHomeLeftBar/vets",
-            "page": "",
-            "icon": "caseHistoryIcon",
-            "imgType": "FA",
-            "url": "veterinarianview/vpVeterenarians",
-            "type": "INTERNAL",
-            "target": "",
-            "rel": "",
-            "routerLink": "/h/veterinarianview/vpVeterenarians",
-            "code": "vets"
-        },
-        {
-            "label": "Owners",
-            "path": "/home/vpHome/vsHomeLeftBar/owners",
-            "page": "",
-            "icon": "caseHistoryIcon",
-            "imgType": "FA",
-            "url": "ownerlandingview/vpOwners",
-            "type": "INTERNAL",
-            "target": "",
-            "rel": "",
-            "routerLink": "/h/ownerlandingview/vpOwners",
-            "code": "owners"
-        },
-        {
-            "label": "Pets",
-            "path": "/home/vpHome/vsHomeLeftBar/pets",
-            "page": "",
-            "icon": "caseHistoryIcon",
-            "imgType": "FA",
-            "url": "petview/vpAllPets",
-            "type": "INTERNAL",
-            "target": "",
-            "rel": "",
-            "routerLink": "/h/petview/vpAllPets",
-            "code": "pets"
-        },
-        {
-            "label": "Notes",
-            "path": "/home/vpHome/vsHomeLeftBar/notes",
-            "page": "",
-            "icon": "notesIcon",
-            "imgType": "FA",
-            "url": "petclinicdashboard/vpNotes",
-            "type": "INTERNAL",
-            "target": "",
-            "rel": "",
-            "routerLink": "/h/petclinicdashboard/vpNotes",
-            "code": "notes"
-        }
-    ],
+    "menuPanel": {
+        "align": "DEFAULT",
+        "code": "vmpMain",
+        "menuItems": [
+            {
+                "label": "Home",
+                "path": "/home/vpHome/vsHomeLeftBar/home",
+                "page": "",
+                "icon": "tasksIcon",
+                "imgType": "FA",
+                "url": "petclinicdashboard/vpDashboard",
+                "type": "INTERNAL",
+                "target": "",
+                "rel": "",
+                "routerLink": "/h/petclinicdashboard/vpDashboard",
+                "code": "home"
+            },
+            {
+                "label": "Veterinarians",
+                "path": "/home/vpHome/vsHomeLeftBar/vets",
+                "page": "",
+                "icon": "caseHistoryIcon",
+                "imgType": "FA",
+                "url": "veterinarianview/vpVeterenarians",
+                "type": "INTERNAL",
+                "target": "",
+                "rel": "",
+                "routerLink": "/h/veterinarianview/vpVeterenarians",
+                "code": "vets"
+            },
+            {
+                "label": "Owners",
+                "path": "/home/vpHome/vsHomeLeftBar/owners",
+                "page": "",
+                "icon": "caseHistoryIcon",
+                "imgType": "FA",
+                "url": "ownerlandingview/vpOwners",
+                "type": "INTERNAL",
+                "target": "",
+                "rel": "",
+                "routerLink": "/h/ownerlandingview/vpOwners",
+                "code": "owners"
+            },
+            {
+                "label": "Pets",
+                "path": "/home/vpHome/vsHomeLeftBar/pets",
+                "page": "",
+                "icon": "caseHistoryIcon",
+                "imgType": "FA",
+                "url": "petview/vpAllPets",
+                "type": "INTERNAL",
+                "target": "",
+                "rel": "",
+                "routerLink": "/h/petview/vpAllPets",
+                "code": "pets"
+            },
+            {
+                "label": "Notes",
+                "path": "/home/vpHome/vsHomeLeftBar/notes",
+                "page": "",
+                "icon": "notesIcon",
+                "imgType": "FA",
+                "url": "petclinicdashboard/vpNotes",
+                "type": "INTERNAL",
+                "target": "",
+                "rel": "",
+                "routerLink": "/h/petclinicdashboard/vpNotes",
+                "code": "notes"
+            }
+        ],
+    },
     "topBar": {
         "branding": {
             "logo": {
