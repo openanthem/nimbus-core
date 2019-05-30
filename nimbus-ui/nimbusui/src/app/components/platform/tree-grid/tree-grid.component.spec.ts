e1fd7470593d38516669942bb443752f02675d4b
lopezton-anthem <antoniotony.lopez@anthem.com>
2019-05-30 10:22:01 -0500

    updating copyright date

--- nimbus-ui/nimbusui/src/app/components/platform/tree-grid/tree-grid.component.spec.ts
+++ nimbus-ui/nimbusui/src/app/components/platform/tree-grid/tree-grid.component.spec.ts
@@ -116,7 +116,7 @@ import { InputMaskComp } from './../form/elements/input-mask.component';
 import { TreeGrid } from './tree-grid.component';
 /**
  * @license
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2016-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
