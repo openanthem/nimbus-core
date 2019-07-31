27977558f9523000e8785474369f7804b929e65f
Sandeep Mantha <sandeep.mantha@anthem.com>
2019-07-31 14:24:36 -0400

    filter support for ZonedDateTime

--- nimbus-test/src/test/java/com/antheminc/oss/nimbus/domain/cmd/exec/internal/DefaultActionExecutorSearchHttpTest.java
+++ nimbus-test/src/test/java/com/antheminc/oss/nimbus/domain/cmd/exec/internal/DefaultActionExecutorSearchHttpTest.java
@@ -19,6 +19,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -646,5 +648,70 @@ public class DefaultActionExecutorSearchHttpTest extends AbstractFrameworkIngera
 		mongo.save(cu, "clientuser");
 		
 		return loginId;
+	}
+	
+	@Test
+	public void t06_GridZoneDateTimeFilter() throws Exception {
+		String userLoginId = createClientUserWithRoles("superman","intake","clinician");
+		ZoneId zoneId = ZoneId.of("UTC");
+		  
+		SampleCoreEntityAccess scea = new SampleCoreEntityAccess();
+		scea.setId(1L);
+		scea.setAttr_String("test1_string1");
+		scea.setAttr_String2("test2_string2");
+		scea.setAttr_LocalDate1(LocalDate.now());
+		scea.setAttr_ZonedDateTime1(ZonedDateTime.of(2019, 07, 19, 20, 10, 10, 1234, zoneId));
+		
+		SampleCoreNestedEntity nestedEntity = new SampleCoreNestedEntity();
+		nestedEntity.setNested_attr_String("nested_test1");
+		scea.setAccessConditional_Contains_Hidden1(nestedEntity);
+		
+		SampleCoreEntityAccess scea2 = new SampleCoreEntityAccess();
+		scea2.setId(2L);
+		scea2.setAttr_String("test2_string1");
+		scea2.setAttr_String2("test2_string2");
+		scea2.setAttr_LocalDate1(LocalDate.now().plusDays(2));
+		scea2.setAttr_ZonedDateTime1(ZonedDateTime.of(2019, 07, 20, 20, 10, 10, 1234, zoneId));
+		
+		SampleCoreNestedEntity nestedEntity2 = new SampleCoreNestedEntity();
+		nestedEntity2.setNested_attr_String("mested_test1");
+		scea2.setAccessConditional_Contains_Hidden1(nestedEntity2);
+		
+		mongo.save(scea, "sample_core_access");
+		mongo.save(scea2, "sample_core_access");
+		
+		
+		Param<?> p = excuteNewConfigView(userLoginId);
+		assertNotNull(p);
+		
+		Long refId = p.findStateByPath("/.m/id");
+		
+		final MockHttpServletRequest gridRequest = MockHttpRequestBuilder
+				.withUri(VIEW_PARAM_ACCESS_ROOT)
+				.addRefId(refId)
+				.addNested("/vpSampleCoreEntityAccess/vtSampleCoreEntityAccess/vsSamplePageCoreEntityAccess/vgSamplePageCoreEntities")
+				//.addParam("pageCriteria", "pageSize=5&page=0&sortBy=attr_String,ASC")
+				.addParam("pageSize", "5")
+				.addParam("page", "0")
+				.addParam("sortBy", "attr_String,ASC")
+				.addAction(Action._get)
+				.getMock();
+		final Object gridResponse = controller.handlePost(gridRequest, "{\"filters\": [{\"code\":\"attr_ZonedDateTime1\", \"value\":\"2019-07-19T04:00:00.000Z\"}]}");
+		//final Object gridResponse = controller.handlePost(gridRequest, null);
+		assertNotNull(gridResponse);
+		
+		List<Output<?>> outputs = MultiOutput.class.cast(Holder.class.cast(gridResponse).getState()).getOutputs();
+		
+		assertNotNull(outputs);
+		
+		for(Output<?> op: outputs) {
+			if(op.getValue() instanceof ListParam<?>) {
+				ListParam<?> param = (ListParam<?>)op.getValue();
+				Page<?> pg = param.getPage();
+				assertNotNull(pg);
+				assertNotNull(pg.getContent());
+				assertEquals(1, pg.getContent().size());
+			}
+		}
 	}
 }
