3e3a90cfa59162c966a30b1224438401a22771cb
Sandeep Mantha <sandeep.mantha@anthem.com>
2019-10-16 14:36:21 -0400

    transient param changes + test cases

--- nimbus-test/src/main/java/com/antheminc/oss/nimbus/test/scenarios/s0/core/SecondLevelNestedModel.java
+++ nimbus-test/src/main/java/com/antheminc/oss/nimbus/test/scenarios/s0/core/SecondLevelNestedModel.java
@@ -15,45 +15,23 @@
  */
 package com.antheminc.oss.nimbus.test.scenarios.s0.core;
 
-import org.springframework.data.annotation.Transient;
-
-import com.antheminc.oss.nimbus.domain.defn.Domain;
-import com.antheminc.oss.nimbus.domain.defn.Domain.ListenerType;
 import com.antheminc.oss.nimbus.domain.defn.Model;
-import com.antheminc.oss.nimbus.domain.defn.Repo;
-import com.antheminc.oss.nimbus.entity.AbstractEntity;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 /**
  * @author Sandeep Mantha
  *
  */
 
-@Domain(value="testmodel_core", includeListeners={ListenerType.persistence, ListenerType.update})
-@Repo(value=Repo.Database.rep_mongodb)
-@Getter @Setter @ToString(callSuper=true)
-public class TestModel extends AbstractEntity.IdLong {
+@Model
+@Getter @Setter
+public class SecondLevelNestedModel {
+	
+	private String nested3_attr1;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	private String attr1;
-
-	private String attr2;
-
-	@Transient
-	private String attr3;
-
-	@Transient
-	private String attr4;
-
-	@Transient
-	private InnerTestModel innerTestModel;
+	private String nested3_attr2;
 	
 	
 }
