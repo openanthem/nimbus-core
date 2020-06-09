4b5f6d26972fa4fb3908db6a09c858c3a9ac0c7e
Dash <AG34346@ad.wellpoint.com>
2020-06-09 14:08:18 +0530

    Vaild Assertion Data

--- nimbus-test/src/test/java/com/antheminc/oss/nimbus/domain/model/state/repo/db/CovidData.java
+++ nimbus-test/src/test/java/com/antheminc/oss/nimbus/domain/model/state/repo/db/CovidData.java
@@ -5,20 +5,21 @@ import com.antheminc.oss.nimbus.domain.defn.Domain.ListenerType;
 import com.antheminc.oss.nimbus.domain.defn.Repo;
 import com.antheminc.oss.nimbus.entity.AbstractEntity;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Domain(value="covid19", includeListeners={ListenerType.persistence, ListenerType.update})
 @Repo(value=Repo.Database.rep_mongodb, cache=Repo.Cache.rep_device)
-@Getter @Setter @ToString(callSuper=true)
+@Getter @Setter @ToString(callSuper=true) @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode(callSuper = true)
 public class CovidData extends AbstractEntity.IdLong{
 	private static final long serialVersionUID = 1L;
 	
 	private String country;
 	private int cases;
 	private String continent;
-
-	
 
 }
--- nimbus-test/src/test/java/com/antheminc/oss/nimbus/domain/model/state/repo/db/MongoSearchByQueryOperationTest.java
+++ nimbus-test/src/test/java/com/antheminc/oss/nimbus/domain/model/state/repo/db/MongoSearchByQueryOperationTest.java
@@ -3,7 +3,9 @@ package com.antheminc.oss.nimbus.domain.model.state.repo.db;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -19,67 +21,66 @@ import com.antheminc.oss.nimbus.support.Holder;
 import com.antheminc.oss.nimbus.test.domain.support.utils.ExtractResponseOutputUtils;
 import com.antheminc.oss.nimbus.test.domain.support.utils.MockHttpRequestBuilder;
 
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MongoSearchByQueryOperationTest extends AbstractFrameworkIngerationPersistableTests {
-	
+
 	protected static final String MSQ_ERR_DOMAIN_ALIAS = "testmongosearchbyqueryoperationfailmodel";
-	protected static final String MSQ_ERR_PARAM_ROOT = PLATFORM_ROOT + "/" + MSQ_ERR_DOMAIN_ALIAS;	
-	
-	
-	
+	protected static final String MSQ_ERR_PARAM_ROOT = PLATFORM_ROOT + "/" + MSQ_ERR_DOMAIN_ALIAS;
+
 	@Before
 	public void load() {
 		CovidData covidData1 = new CovidData();
-		covidData1.setCountry("india");
+		covidData1.setCountry("India");
 		covidData1.setCases(199613);
 		covidData1.setContinent("Asia");
 		covidData1.setId(1L);
-		mongo.save(covidData1,"covid19");
-		
+		mongo.save(covidData1, "covid19");
+
 		CovidData covidData2 = new CovidData();
-		covidData2.setCountry("Indonesia");
+		covidData2.setCountry("China");
 		covidData2.setCases(27549);
 		covidData2.setContinent("Asia");
 		covidData2.setId(2L);
-		mongo.save(covidData2,"covid19");
-		
+		mongo.save(covidData2, "covid19");
+
 		CovidData covidData3 = new CovidData();
 		covidData3.setCountry("Iran");
 		covidData3.setCases(157562);
 		covidData3.setContinent("Asia");
 		covidData3.setId(3L);
-		mongo.save(covidData3,"covid19");
+		mongo.save(covidData3, "covid19");
 
 	}
-	
+
 	@Test
 	@SuppressWarnings("unchecked")
 	public void search_by_aggregation() {
-		MockHttpServletRequest request = MockHttpRequestBuilder.withUri("/anthem/thebox/p/testmongosearchbyqueryoperationfailmodel")
-				.addAction(Action._new)
-				.getMock();
-	
-		Holder<MultiOutput> holder = (Holder<MultiOutput>)controller.handlePost(request, null);
-		Long domainRoot_refId  = ExtractResponseOutputUtils.extractDomainRootRefId(holder);
+		MockHttpServletRequest request = MockHttpRequestBuilder
+				.withUri("/anthem/thebox/p/testmongosearchbyqueryoperationfailmodel").addAction(Action._new).getMock();
+
+		Holder<MultiOutput> holder = (Holder<MultiOutput>) controller.handlePost(request, null);
+		Long domainRoot_refId = ExtractResponseOutputUtils.extractDomainRootRefId(holder);
 		assertNotNull(domainRoot_refId);
-		
-		MockHttpServletRequest request2 = MockHttpRequestBuilder.withUri("/anthem/thebox/p/testmongosearchbyqueryoperationfailmodel:1/action2/_get")
-				.getMock();
-		
-		holder = (Holder<MultiOutput>)controller.handlePost(request2, null);	
-		
-		request2 = MockHttpRequestBuilder.withUri("/anthem/thebox/p/testmongosearchbyqueryoperationfailmodel:1/result/_get")
-				.getMock();
-		
-		holder = (Holder<MultiOutput>)controller.handlePost(request2, null);	
-		
-		Param<?> response = (Param<?>)holder.getState().getSingleResult();
-		List<CovidData> covidData = (List<CovidData>)response.getState();
-	
-		
+
+		MockHttpServletRequest request2 = MockHttpRequestBuilder
+				.withUri("/anthem/thebox/p/testmongosearchbyqueryoperationfailmodel:1/action2/_get").getMock();
+
+		holder = (Holder<MultiOutput>) controller.handlePost(request2, null);
+
+		request2 = MockHttpRequestBuilder
+				.withUri("/anthem/thebox/p/testmongosearchbyqueryoperationfailmodel:1/result/_get").getMock();
+
+		holder = (Holder<MultiOutput>) controller.handlePost(request2, null);
+
+		Param<?> response = (Param<?>) holder.getState().getSingleResult();
+		List<CovidData> covidData = (List<CovidData>) response.getState();
+		List<String> actual = covidData.stream().map(CovidData::getCountry).collect(Collectors.toList());
+
+		List<String> expected = Arrays.asList(new String[] { "India", "China", "Iran" });
+
+		assertEquals(expected, actual);
 		// Add valid assertion here to account for collated data
-	
+
 	}
-	
+
 }
