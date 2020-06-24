cb84ea572eea930e76c6f46815a89055dca9d39b
Kashyap, Akancha <Akancha.Kashyap@anthem.com>
2020-06-24 15:37:40 -0400

    Changed mongo fileDownload to fetch from Id instead of Name since Name is not neccessarily unique

--- nimbus-core/src/main/java/com/antheminc/oss/nimbus/entity/fileUpload/FileUploadMongo.java
+++ nimbus-core/src/main/java/com/antheminc/oss/nimbus/entity/fileUpload/FileUploadMongo.java
@@ -16,63 +16,73 @@ import com.antheminc.oss.nimbus.context.BeanResolverStrategy;
 import com.antheminc.oss.nimbus.domain.defn.Domain;
 import com.antheminc.oss.nimbus.domain.defn.Domain.ListenerType;
 import com.antheminc.oss.nimbus.domain.model.state.repo.db.mongo.DefaultMongoModelRepository;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
 import com.mongodb.client.gridfs.model.GridFSFile;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Domain(value="fileUpload", includeListeners={ListenerType.update})
+@Domain(value = "fileUpload", includeListeners = { ListenerType.update })
 
-@Getter @Setter @ToString
-public class FileUploadMongo implements FileUploadService{
-	
-	
-	
+@Getter
+@Setter
+@ToString
+public class FileUploadMongo implements FileUploadService {
+
 	private MongoTemplate mongoTemplate;
-	
-	private GridFsTemplate gridFsTemplate ;
-	
 
-	
+	private GridFsTemplate gridFsTemplate;
+
 	public FileUploadMongo(BeanResolverStrategy beanResolver) {
-		DefaultMongoModelRepository defaultMongoModelRepository = beanResolver.get(DefaultMongoModelRepository.class,"rep_mongodb");
+		DefaultMongoModelRepository defaultMongoModelRepository = beanResolver.get(DefaultMongoModelRepository.class,
+				"rep_mongodb");
 		this.mongoTemplate = (MongoTemplate) defaultMongoModelRepository.getMongoOps();
-		this.gridFsTemplate =  new GridFsTemplate(mongoTemplate.getMongoDbFactory(),mongoTemplate.getConverter());
-		
+		this.gridFsTemplate = new GridFsTemplate(mongoTemplate.getMongoDbFactory(), mongoTemplate.getConverter());
+
 	}
-	
+
 	@Override
 	public String upload(MultipartFile uploadFile) {
-		
+
 		ObjectId externalId;
 		try {
-			externalId = gridFsTemplate.store(uploadFile.getInputStream(),uploadFile.getOriginalFilename());
+			externalId = gridFsTemplate.store(uploadFile.getInputStream(), uploadFile.getOriginalFilename());
 			return externalId.toString();
 		} catch (IOException e) {
-			throw new FrameworkRuntimeException("Unable to upload File to GridFs with FileName "+uploadFile.getOriginalFilename(),e );
+			throw new FrameworkRuntimeException(
+					"Unable to upload File to GridFs with FileName " + uploadFile.getOriginalFilename(), e);
 		}
-		
+
 	}
 
 	@Override
 	public InputStream download(String downloadFileExternalId) {
 		Query q = new Query().addCriteria(Criteria.where("_id").is(downloadFileExternalId));
 		GridFSFile gridFsFile = gridFsTemplate.findOne(q);
-		if(null==gridFsFile) {
-			throw new FrameworkRuntimeException("Unable to find file with Id "+downloadFileExternalId );
-		}
-		else {
-			GridFsResource resource = gridFsTemplate.getResource(gridFsFile.getFilename());
+		if (null == gridFsFile) {
+			throw new FrameworkRuntimeException("Unable to find file with Id " + downloadFileExternalId);
+		} else {
+			GridFsResource resource = new GridFsResource(gridFsFile,
+					getGridFs().openDownloadStream(gridFsFile.getObjectId()));
 			try {
 
 				return resource.getInputStream();
-				
+
 			} catch (Exception e) {
-				throw new FrameworkRuntimeException("Unable to write located file  with Id :"+downloadFileExternalId+" to InputStream "+e );
+				throw new FrameworkRuntimeException(
+						"Unable to write located file  with Id :" + downloadFileExternalId + " to InputStream " + e);
 			}
 		}
-		
+
+	}
+
+	private GridFSBucket getGridFs() {
+
+		MongoDatabase db = mongoTemplate.getDb();
+		return GridFSBuckets.create(db);
 	}
 
 }
